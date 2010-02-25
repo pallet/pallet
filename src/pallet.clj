6a89f5dd3aa3b0ace04ba14a87c56f14779b0f47
Hugo Duncan <hugo_duncan@yahoo.com>
2010-02-24 22:27:14 -0500

    Documentation update.  Added top-level pallet namespace, refering all the sub-namespaces

--- src/pallet.clj
+++ src/pallet.clj
@@ -1,31 +1,32 @@
-# Pallet
-
+(ns
+    #^{:author "Hugo Duncan"
+       :doc "
 Pallet is used to provision configured compute nodes, and aims to solve the
 problem of providing a consistently configured running image across a range of
-clouds.  It is designed for use from the [Clojure](http://clojure.org) REPL, and
+clouds.  It is designed for use from the Clojure (http://clojure.org) REPL, and
 from clojure scripts.
 
-It uses [jclouds](http://github.com/jclouds/jclouds) to gain portable access to
-different cloud providers.  While jclouds solves the issue of creating,
+It uses jclouds (http://github.com/jclouds/jclouds) to gain portable access to
+different cloud providers.  While jclouds solves the issue of creating
 destroying and configuring cloud level access to nodes, it does not address the
 differences in images used across providers.  This is what Pallet adds.
 
 Pallet works with a declaritive map for specifying the number of nodes with a
 given tag.  Each tag is used to look up a machine image template specification
-(in jclouds), and to lookup configuration information.  The converge function
-then tries to bring you compute servers into alignment with your declared counts
-and configurations.
+and to lookup configuration information.  The converge function then tries to
+bring you compute servers into alignment with your declared counts and
+configurations.
 
 The bootstrap process for new compute nodes is configurable.  At the moment
 there is the possibility of using scripts, which are specialised by tag or
 operating system family, and of using installing an admin user with sudo
 permissions, using the specified username and password.  Script templates exist
-for ensuring a functional resolv configuration, package manager update,
+for ensuring a functional resolv configuration, package manager update
 installation of rubygems, etc, and can be added to freely.
 
 An installed admin user can be used to execute chef cookbooks using 'chef-solo'.
 Once the nodes are bootstrapped, and fall all existing nodes, the configured
-node information is written to the "compute-nodes" cookbook before chef is run,
+node information is written to the 'compute-nodes' cookbook before chef is run
 and this provides a :compute_nodes attribute.  The compute-nodes cookbook is
 expected to exist in the site-cookbooks of the chef-repository you specify with
 `with-chef-repository`. `chef-solo` is then run with chef repository you have
@@ -33,11 +34,11 @@ specified using the node tag as a configuration target.
 
 This can also be used to bootstrap chef-server and chef client installation.
 
-[API documentation](http://hugoduncan.github.com/pallet) is available.
+API documentation (http://hugoduncan.github.com/pallet) is available.
 
 ## Usage
 
-[See demo documentation](http://hugoduncan.github.com/pallet/demo-api.html).
+See demo documentation (http://hugoduncan.github.com/pallet/demo-api.html).
 
 ## Todo
 
@@ -48,10 +49,13 @@ Add templating by cloud provider.
 
 ## Installation
 
-Installation is with [Leiningen](http://github.com/technomancy/leiningen).  Add
-`[pallet "0.0.1-SNAPSHOT"]` to your :dependencies in project.clj.
+Installation is with Leiningen (http://github.com/technomancy/leiningen).  Add
+`[pallet \"0.0.1-SNAPSHOT\"]` to your :dependencies in project.clj.
 
-## See also
-[chef](http://wiki.opscode.com/display/chef/Home),
-[crane](http://github.com/bradford/crane),
-[jclouds](http://github.com/jclouds/jclouds)
+" :see-also [["chef" "http://wiki.opscode.com/display/chef/Home"]
+             ["crane" "http://github.com/bradford/crane"]
+             ["jclouds" "http://github.com/jclouds/jclouds"]]}
+  pallet
+  (:require pallet.core pallet.utils pallet.compute pallet.bootstrap pallet.chef))
+
+(refer 'pallet.core 'pallet.utils 'pallet.compute 'pallet.bootstrap 'pallet.chef)
