bff034551d80696bc39954f85558add31ca6c7fe
Hugo Duncan <hugo_duncan@yahoo.com>
2010-03-29 21:08:40 -0400

    add crates for ruby, rubygems and chef.  added remote file resource, and conditional resources

--- src/pallet/crate/automated_admin_user.clj
+++ src/pallet/crate/automated_admin_user.clj
@@ -5,10 +5,14 @@
         [pallet.utils :only [default-public-key-path]]))
 
 (defn automated-admin-user
-  "Builds a user for use in remote-admin automation.  The user is given
+  "Builds a user for use in remote-admin automation. The user is given
   permission to sudo without password, so that passwords don't have to appear
   in scripts, etc."
-  ([username] (automated-admin-user username (default-public-key-path)))
+  ([] (automated-admin-user
+       (. System getProperty "user.name")
+       (default-public-key-path)))
+  ([username] (automated-admin-user
+               username (default-public-key-path)))
   ([username public-key-path]
      (user username :create-home true :shell :bash)
      (authorize-key username (slurp public-key-path))
