26c616d70e8034fdd1d508f9839d189368d852f0
Hugo Duncan <hugo_duncan@yahoo.com>
2010-04-17 15:03:00 -0400

    Improved test to allow running on wider range of machines

--- test/pallet/utils_test.clj
+++ test/pallet/utils_test.clj
@@ -1,10 +1,16 @@
 (ns pallet.utils-test
   (:use [pallet.utils] :reload-all)
   (:use clojure.test
+        clojure.contrib.logging
         pallet.test-utils))
 
 (deftest system-test
-  (is (= {:exit 0 :out "" :err ""} (system "/usr/bin/true"))))
+  (cond
+   (.canRead (java.io.File. "/usr/bin/true")) (is (= {:exit 0 :out "" :err ""}
+                                                     (system "/usr/bin/true")))
+   (.canRead (java.io.File. "/bin/true")) (is (= {:exit 0 :out "" :err ""}
+                                                 (system "/bin/true")))
+   :else (warn "Skipping system-test")))
 
 (deftest bash-test
   (is (= {:exit 0 :out "fred\n" :err ""} (bash "echo fred"))))
