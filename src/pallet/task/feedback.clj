4f8f7ee647060f184e78d8ae3fdec95a8e612a0e
Hugo Duncan <hugo_duncan@yahoo.com>
2010-04-30 13:23:35 -0400

    Added initial feedback task

---
+++ src/pallet/task/feedback.clj
@@ -0,0 +1,28 @@
+(ns pallet.task.feedback
+  "Send feedback to the pallet project.  Arguments will be sent as text."
+  (:require
+   [clojure.contrib.http.agent :as agent]
+   [clojure.contrib.http.connection :as connection]
+   pallet.compat))
+
+(pallet.compat/require-contrib)
+
+(def heynote-project "pallet")
+(def heynote-url "http://orcloud-heynote.appspot.com/")
+
+(defn feedback
+  {:no-service-required true}
+  [& args]
+  (let [msg (apply str (interpose " " args))
+        agent (agent/http-agent
+               (str heynote-url "item/new")
+               :method "POST"
+               :body (json-write/json-str
+                      {:project heynote-project
+                       :text msg})
+               :headers {"Content-type" "application/json"})]
+    (agent/result agent)
+    (println
+     (if (agent/client-error? agent)
+       "There was a problem sending your feedback."
+       "Feedback sent."))))
