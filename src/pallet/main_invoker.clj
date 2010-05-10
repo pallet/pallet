30e5fe2c1412b75f2daa25001ba4a53cb176ce07
Hugo Duncan <hugo_duncan@yahoo.com>
2010-05-10 15:32:22 -0400

    Added extra logging

--- src/pallet/main_invoker.clj
+++ src/pallet/main_invoker.clj
@@ -5,22 +5,42 @@
   (:require
    [org.jclouds.compute :as jclouds]
    [clojure.contrib.logging :as logging]
+   [pallet.utils :as utils]
    [pallet.maven :as maven]
    [pallet.main :as main]))
 
 (def default-service-opts [:log4j :enterprise :ssh])
 
+(defn log-info
+  []
+  (logging/debug (format "OS              %s %s"
+                         (System/getProperty "os.name")
+                         (System/getProperty "os.version")))
+  (logging/debug (format "Arch            %s" (System/getProperty "os.arch")))
+  (logging/debug (format "Admin user      %s" (:username utils/*admin-user*)))
+  (let [private-key-path (:private-key-path utils/*admin-user*)
+        public-key-path (:public-key-path utils/*admin-user*)]
+    (logging/debug
+     (format "private-key-path %s %s" private-key-path
+             (.canRead (java.io.File. private-key-path))))
+    (logging/debug
+     (format "public-key-path %s %s" public-key-path
+             (.canRead (java.io.File. public-key-path))))))
+
 (defn invoke
   [service user key task params]
+  (log-info)
   (let [[service user key] (if service
                              [service user key]
                              (maven/credentials))]
     (if service
-      (let [compute (apply jclouds/compute-service
-                           (concat [service user key] default-service-opts))]
-        (logging/debug (format "Running os %s@%s" user service))
-        (jclouds/with-compute-service [compute]
-          (apply task params)))
+      (do
+        (logging/debug (format "Running as      %s@%s" user service))
+        (let [compute (apply jclouds/compute-service
+                             (concat [service user key] default-service-opts))]
+
+          (jclouds/with-compute-service [compute]
+            (apply task params))))
       (do
         (println "Error: no credentials supplied\n\n")
         (apply (main/resolve-task "help") [])))))
