67c3dbe19d725e10baf62d369185ada2865e125c
Adrian Cole <adrian@jclouds.org>
2010-05-25 15:58:40 -0600

    fixed to use nodes-with-details to ensure NodeMetadata is returned instead of ComputeMetadata

--- pallet/src/server/performance.clj
+++ pallet/src/server/performance.clj
@@ -15,11 +15,11 @@
   (exec-script/exec-script
    (stevedore/script
     ("/usr/local/bin/iozone" -Rb
-     ~(str "/var/lib/iozone/" (stevedore/script @(hostname :s true)) ".xls")
-     -s "2g" -i 0 -i 1 -i 2 -f "/mnt/testfile" -r "32k" -g "1G"))))
+     ~(str "/var/lib/iozone/" (stevedore/script (hostname :s true)) ".xls")
+     -s "2g" -i 0 -i 1 -i 2 -f "/mnt/testfile" -r "32k" -g "2G"))))
 
 (core/defnode small
-  [:ubuntu :X86_64 :smallest :image-name-matches ".*" :os-description-matches "[^J]+9.10[^32]+"]
+  []
   :bootstrap [(package/package-manager :update)
               (automated-admin-user/automated-admin-user)]
   :configure [(iozone/iozone)]
