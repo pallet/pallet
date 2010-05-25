18372a0cda15707dddcf98f105b65de55b2bc59e
Hugo Duncan <hugo_duncan@yahoo.com>
2010-05-24 22:20:01 -0400

    fixed issue with exclude symbol being syntax quoted

--- src/pallet/repl.clj
+++ src/pallet/repl.clj
@@ -15,15 +15,16 @@
 (defmacro use-pallet
   "Macro that will use pallet's namespaces, to provide an easy to access REPL."
   []
-  `(do
-     (use 'org.jclouds.compute)
-     (use 'pallet.utils)
-     (use ['pallet.compute :exclude ['make-node]])
-     (use 'pallet.core)
-     (use 'pallet.maven)
-     (use 'pallet.resource)
-     (use 'pallet.resource.package)
-     (use 'pallet.crate.automated-admin-user)
-     (use 'pallet.crate.public-dns-if-no-nameserver)
-     (use 'pallet.task.feedback)
-     (use 'clj-ssh.ssh)))
+  '(do
+     (clojure.core/use
+      'org.jclouds.compute
+      'pallet.utils
+      '[pallet.compute :exclude [make-node]]
+      'pallet.core
+      'pallet.maven
+      'pallet.resource
+      'pallet.resource.package
+      'pallet.crate.automated-admin-user
+      'pallet.crate.public-dns-if-no-nameserver
+      'pallet.task.feedback
+      'clj-ssh.ssh)))
