b1d76c2e45eedd0bc9dde090e55623e88f0064c0
Hugo Duncan <hugo_duncan@yahoo.com>
2010-07-20 22:29:42 -0400

    Extended iptables to handle all tables

--- src/pallet/crate/iptables.clj
+++ src/pallet/crate/iptables.clj
@@ -2,81 +2,105 @@
   "Crate for managing iptables"
   (:require
    pallet.target
+   [pallet.stevedore :as stevedore]
    pallet.resource
    pallet.resource.remote-file
    pallet.resource.file
-   [clojure.contrib.string :as string]))
+   [clojure.contrib.string :as string]
+   [clojure.contrib.logging :as logging]))
 
 (def prefix
-     "*filter
-:INPUT ACCEPT
+     {"filter" ":INPUT ACCEPT
 :FORWARD ACCEPT
 :OUTPUT ACCEPT
 :FWR -
 -A INPUT -j FWR
 -A FWR -i lo -j ACCEPT
-")
+"})
 (def suffix
-     "# Rejects all remaining connections with port-unreachable errors.
+     {"filter" "# Rejects all remaining connections with port-unreachable errors.
 -A FWR -p tcp -m tcp --tcp-flags SYN,RST,ACK SYN -j REJECT --reject-with icmp-port-unreachable
 -A FWR -p udp -j REJECT --reject-with icmp-port-unreachable
 COMMIT
-")
+"})
 
+(defn restore-iptables
+  [[table rules]]
+  (pallet.stevedore/script
+   (var tmp @(mktemp iptablesXXXX))
+   ~(pallet.resource.remote-file/remote-file*
+     "$tmp" :content rules)
+   ~(pallet.stevedore/checked-script
+     "Restore IPtables"
+     ("/sbin/iptables-restore" "-t" ~table < @tmp))
+   (rm @tmp)))
+
+(defn format-iptables
+  [tables]
+  (string/join \newline (map #(str "*" (first %) \newline (second %)) tables)))
 
 (defn iptables*
   "Combine all iptables rules"
   [args]
-  (let [table (string/join
-               \newline [prefix
-                         (string/join
-                          \newline
-                          (map #(string/join \newline %) args))
-                         suffix])
+  (let [args (group-by first args)
+        tables (into
+                {}
+                (map
+                 #(vector (first %)
+                          (string/join
+                           \newline (filter
+                                     (complement nil?)
+                                     [(prefix (first %))
+                                      (string/join
+                                       \newline
+                                       (map (fn [x]
+                                              (string/join \newline x)) (rest (second %))))
+                                      (suffix (first %))]))) args))
         packager (pallet.target/packager)]
     (condp = packager
-        :aptitude (pallet.stevedore/script
-                   (var tmp @(mktemp iptablesXXXX))
-                   ~(pallet.resource.remote-file/remote-file*
-                     "$tmp" :content table)
-                   ~(pallet.stevedore/checked-script
-                     "Restore IPtables"
-                     ("/sbin/iptables-restore" < @tmp))
-                   (rm @tmp))
-        :yum (pallet.resource.remote-file/remote-file
+        :aptitude (stevedore/do-script* (map restore-iptables tables))
+        :yum (pallet.resource.remote-file/remote-file*
               "/etc/sysconfig/iptables"
               :mode "0755"
-              :content table))))
+              :content (format-iptables tables)))))
 
 (pallet.resource/defaggregate iptables-rule
   "Define a rule for the iptables. The argument should be a string containing an
 iptables configuration line (cf. arguments to an iptables invocation)"
-  iptables* [config-line])
+  iptables* [table config-line])
 
 (defn iptables-accept-established
   "Accept established connections"
   []
-  (iptables-rule "-A FWR -m state --state RELATED,ESTABLISHED -j ACCEPT"))
+  (iptables-rule "filter" "-A FWR -m state --state RELATED,ESTABLISHED -j ACCEPT"))
 
 (defn iptables-accept-icmp
   "Accept ICMP"
   []
-  (iptables-rule "-A FWR -p icmp -j ACCEPT"))
+  (iptables-rule "filter" "-A FWR -p icmp -j ACCEPT"))
 
 (defn iptables-accept-port
   "Accept specific port, by default for tcp."
   ([port] (iptables-accept-port port "tcp"))
   ([port protocol]
      (iptables-rule
-      (format "-A FWR -p %s --dport %s -j ACCEPT"
-              protocol port))))
+      "filter"
+      (format "-A FWR -p %s --dport %s -j ACCEPT" protocol port))))
+
+(defn iptables-redirect-port
+  "Redirect a specific port, by default for tcp."
+  ([from-port to-port] (iptables-accept-port from-port to-port "tcp"))
+  ([from-port to-port protocol]
+     (iptables-rule "nat"
+      (format "-I PREROUTING -p %s --dport %s -j REDIRECT --to-port %s"
+              protocol from-port to-port))))
 
 (defn iptables-throttle
   "Throttle repeated connection attempts.
    http://hostingfu.com/article/ssh-dictionary-attack-prevention-with-iptables"
   ([name port] (iptables-throttle name port "tcp" 60 4))
   ([name port protocol time-period hitcount]
-     (pallet.crate.iptables/iptables-rule
+     (iptables-rule "filter"
       (format
        "-N %s
 -A FWR -p %s --dport %s -m state --state NEW -j %s
--- test/pallet/crate/iptables_test.clj
+++ test/pallet/crate/iptables_test.clj
@@ -9,18 +9,31 @@
   (:use clojure.test
         pallet.test-utils))
 
-(deftest site-test
+(deftest iptables-test
   []
-  (is (= (stevedore/do-script
-          (stevedore/script (var tmp @(mktemp iptablesXXXX)))
-          (pallet.resource.remote-file/remote-file*
-           "$tmp"
-           :content
-           "*filter\n:INPUT ACCEPT\n:FORWARD ACCEPT\n:OUTPUT ACCEPT\n:FWR -\n-A INPUT -j FWR\n-A FWR -i lo -j ACCEPT\n\n\n# Rejects all remaining connections with port-unreachable errors.\n-A FWR -p tcp -m tcp --tcp-flags SYN,RST,ACK SYN -j REJECT --reject-with icmp-port-unreachable\n-A FWR -p udp -j REJECT --reject-with icmp-port-unreachable\nCOMMIT\n")
-          (stevedore/checked-script
-           "Restore IPtables"
-           ("/sbin/iptables-restore" < @tmp))
-          (stevedore/script (rm @tmp)))
-         (target/with-target nil {:tag :n :image [:ubuntu]}
-           (resource/build-resources
-            [] (iptables-rule ""))))))
+  (testing "debian"
+    (is (= (stevedore/do-script
+            (stevedore/script (var tmp @(mktemp iptablesXXXX)))
+            (pallet.resource.remote-file/remote-file*
+             "$tmp"
+             :content
+             ":INPUT ACCEPT\n:FORWARD ACCEPT\n:OUTPUT ACCEPT\n:FWR -\n-A INPUT -j FWR\n-A FWR -i lo -j ACCEPT\n\n\n# Rejects all remaining connections with port-unreachable errors.\n-A FWR -p tcp -m tcp --tcp-flags SYN,RST,ACK SYN -j REJECT --reject-with icmp-port-unreachable\n-A FWR -p udp -j REJECT --reject-with icmp-port-unreachable\nCOMMIT\n")
+            (stevedore/checked-script
+             "Restore IPtables"
+             ("/sbin/iptables-restore" "-t" "filter" < @tmp))
+            (stevedore/script (rm @tmp)))
+           (target/with-target nil {:tag :n :image [:ubuntu]}
+             (resource/build-resources
+              [] (iptables-rule "filter" ""))))))
+  (testing "redhat"
+    (is (= (stevedore/do-script
+            (pallet.resource.remote-file/remote-file*
+             "/etc/sysconfig/iptables"
+             :content
+             "*filter\n:INPUT ACCEPT\n:FORWARD ACCEPT\n:OUTPUT ACCEPT\n:FWR -\n-A INPUT -j FWR\n-A FWR -i lo -j ACCEPT\n\n\n# Rejects all remaining connections with port-unreachable errors.\n-A FWR -p tcp -m tcp --tcp-flags SYN,RST,ACK SYN -j REJECT --reject-with icmp-port-unreachable\n-A FWR -p udp -j REJECT --reject-with icmp-port-unreachable\nCOMMIT\n"
+             :mode "0755"))
+           (target/with-target nil {:tag :n :image [:centos]}
+             (resource/build-resources
+              [] (iptables-rule "filter" "")))))))
+
+
