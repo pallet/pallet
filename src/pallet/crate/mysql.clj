3ea3dc2b84c8fecc38058efee11312665d5590dd
Hugo Duncan <hugo_duncan@yahoo.com>
2010-07-21 22:27:56 -0400

    Fixed mysql install password bug. Added create-database, create-user and grant.  Improved tests

--- src/pallet/crate/mysql.clj
+++ src/pallet/crate/mysql.clj
@@ -1,6 +1,8 @@
 (ns pallet.crate.mysql
   (:require
-   [pallet.resource.package :as package])
+   [pallet.resource.package :as package]
+   [pallet.resource :as resource]
+   [pallet.parameter :as parameter])
   (:use
    [pallet.target :only [packager]]
    [pallet.resource :only [defresource]]
@@ -22,11 +24,13 @@
       :debconf
       (str "mysql-server-5.1 mysql-server/root_password password " root-password)
       (str "mysql-server-5.1 mysql-server/root_password_again password " root-password)
-      (str "mysql-server-5.1 mysql-server/start_on_boot boolean " start-on-boot))
+      (str "mysql-server-5.1 mysql-server/start_on_boot boolean " start-on-boot)
       (str "mysql-server-5.1 mysql-server-5.1/root_password password " root-password)
       (str "mysql-server-5.1 mysql-server-5.1/root_password_again password " root-password)
-      (str "mysql-server-5.1 mysql-server/start_on_boot boolean " start-on-boot)
-      (package/package "mysql-server")))
+      (str "mysql-server-5.1 mysql-server/start_on_boot boolean " start-on-boot))
+     (package/package "mysql-server")
+     (resource/parameters
+      [:mysql :root-password] root-password)))
 
 (deftemplate my-cnf-template
   [string]
@@ -49,6 +53,23 @@
   "Execute a mysql script"
   mysql-script* [username password sql-script])
 
-;yum install php-mysql mysql mysql-server
-;/sbin/chkconfig --levels 235 mysqld on
-;/etc/init.d/mysqld start
+(defn create-database
+  ([name] (create-database name "root" (parameter/lookup :mysql :root-password)))
+  ([name username root-password]
+     (mysql-script
+      username root-password (format "CREATE DATABASE IF NOT EXISTS `%s`" name))))
+
+(defn create-user
+  ([user password]
+     (create-user user password "root" (parameter/lookup :mysql :root-password)))
+  ([user password username root-password]
+     (mysql-script
+      username root-password
+      (format "GRANT USAGE ON *.* TO %s IDENTIFIED BY `%s`" user password))))
+
+(defn grant
+  ([privileges level user]
+     (grant privileges level user "root" (parameter/lookup :mysql :root-password)))
+  ([privileges level user username root-password]
+     (mysql-script
+      username root-password (format "GRANT %s ON %s TO %s" privileges level user))))
--- test/pallet/crate/mysql_test.clj
+++ test/pallet/crate/mysql_test.clj
@@ -2,10 +2,51 @@
   (:use [pallet.crate.mysql] :reload-all)
   (:use [pallet.resource :only [build-resources]]
         clojure.test
-        pallet.test-utils))
+        pallet.test-utils)
+  (:require
+   [pallet.stevedore :as stevedore]
+   [pallet.parameter :as parameter]
+   [pallet.resource :as resource]
+   [pallet.target :as target]
+   [pallet.resource.package :as package]))
 
 (use-fixtures :each with-null-target)
+
+
+(deftest mysql-server-test
+  (parameter/with-parameters [:default]
+    (is (=
+         (stevedore/do-script
+          (package/package-manager*
+           :debconf
+           (str "mysql-server-5.1 mysql-server/root_password password " "pwd")
+           (str "mysql-server-5.1 mysql-server/root_password_again password " "pwd")
+           (str "mysql-server-5.1 mysql-server/start_on_boot boolean " true)
+           (str "mysql-server-5.1 mysql-server-5.1/root_password password " "pwd")
+           (str "mysql-server-5.1 mysql-server-5.1/root_password_again password " "pwd")
+           (str "mysql-server-5.1 mysql-server/start_on_boot boolean " true))
+          "{ debconf-set-selections <<EOF\ndebconf debconf/frontend select noninteractive\ndebconf debconf/frontend seen false\nEOF\n}\n"
+          (package/package* "mysql-server")
+          (resource/parameters*
+           [:mysql :root-password] "pwd"))
+         (target/with-target nil {:tag :n :image [:ubuntu]}
+           (build-resources [] (mysql-server "pwd")))))))
 
 (deftest mysql-conf-test
   (is (= "file=/etc/mysql/my.cnf\ncat > ${file} <<EOF\n[client]\nport = 3306\n\nEOF\nchmod 0440 ${file}\nchown root ${file}\n"
          (build-resources [] (mysql-conf "[client]\nport = 3306\n")))))
+
+(deftest create-database-test
+  (is (= (stevedore/do-script
+          (mysql-script* "root" "pwd" "CREATE DATABASE IF NOT EXISTS `db`"))
+         (build-resources [] (create-database "db" "root" "pwd")))))
+
+(deftest create-user-test
+  (is (= (stevedore/do-script
+          (mysql-script* "root" "pwd" "GRANT USAGE ON *.* TO user IDENTIFIED BY `pw`"))
+         (build-resources [] (create-user "user" "pw" "root" "pwd")))))
+
+(deftest grant-test
+  (is (= (stevedore/do-script
+          (mysql-script* "root" "pwd" "GRANT ALL PRIVILEGES ON `db`.* TO user"))
+         (build-resources [] (grant "ALL PRIVILEGES" "`db`.*" "user" "root" "pwd")))))
