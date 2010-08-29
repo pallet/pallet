e4d50559850f1023365075100e1b98d0feba55d9
Hugo Duncan <hugo_duncan@yahoo.com>
2010-08-28 23:41:14 -0400

    initial version of syslog-ng crate

---
+++ src/pallet/crate/syslog_ng.clj
@@ -0,0 +1,272 @@
+(ns pallet.crate.syslog-ng
+  "Install and configure syslog-ng"
+  (:require
+   [pallet.parameter :as parameter]
+   [pallet.resource :as resource]
+   [pallet.resource.remote-file :as remote-file]
+   [pallet.resource.package :as package]
+   [pallet.resource.user :as user]
+   [clojure.string :as string]))
+
+(defn default-group "adm")
+(defn default-mode "0640")
+
+(def default-options
+  {:chain_hostnames 0
+   :time_reopen 10
+   :time_reap 360
+   :sync 0
+   :log_fifo_size 2048
+   :create_dirs "yes"
+   :group "adm"
+   :dir_group "adm"
+   :perm "0640"
+   :dir_perm "0750"
+   :use_dns "no"
+   :stats_freq 0
+   :bad_hostname "^gconfd$"})
+
+(def default-sources
+  {:s_all {:internal true
+           :unix-stream "/dev/log"
+           :file {:value "/proc/kmsg" :log_prefix "kernel: "}}})
+
+(def default-destinations
+  {:df_auth { :file "/var/log/auth.log" }
+   :df_syslog { :file "/var/log/syslog" }
+   :df_cron { :file "/var/log/cron.log" }
+   :df_daemon { :file "/var/log/daemon.log" }
+   :df_kern { :file "/var/log/kern.log" }
+   :df_lpr { :file "/var/log/lpr.log" }
+   :df_mail { :file "/var/log/mail.log" }
+   :df_user { :file "/var/log/user.log" }
+   :df_uucp { :file "/var/log/uucp.log" }
+   :df_facility_dot_info { :file "/var/log/$FACILITY.info" }
+   :df_facility_dot_notice { :file "/var/log/$FACILITY.notice" }
+   :df_facility_dot_warn { :file "/var/log/$FACILITY.warn" }
+   :df_facility_dot_err { :file "/var/log/$FACILITY.err" }
+   :df_facility_dot_crit { :file "/var/log/$FACILITY.crit" }
+   :df_news_dot_notice { :file {:value "/var/log/news/news.notice"
+                                :owner "news"} }
+   :df_news_dot_err { :file {:value "/var/log/news/news.err" :owner "news"} }
+   :df_news_dot_crit { :file {:value "/var/log/news/news.crit" :owner "news"} }
+   :df_debug { :file "/var/log/debug" }
+   :df_messages { :file "/var/log/messages" }
+   :dp_xconsole { :pipe "/dev/xconsole" }
+   :du_all { :usertty "*" }})
+
+(def default-filters
+  {:f_auth { :facility "auth, authpriv" }
+   :f_syslog { :not { :facility "auth, authpriv" } }
+   :f_cron { :facility "cron" }
+   :f_daemon { :facility "daemon" }
+   :f_kern { :facility "kern" }
+   :f_lpr { :facility "lpr" }
+   :f_mail { :facility "mail" }
+   :f_news { :facility "news" }
+   :f_user { :facility "user" }
+   :f_uucp { :facility "uucp" }
+   :f_at_least_info { :level "info..emerg" }
+   :f_at_least_notice { :level "notice..emerg" }
+   :f_at_least_warn { :level "warn..emerg" }
+   :f_at_least_err { :level "err..emerg" }
+   :f_at_least_crit { :level "crit..emerg" }
+   :f_debug {:and {:level "debug"
+                   :not {:facility "auth, authpriv, news, mail"}}}
+   :f_messages {:and {:level "info,notice,warn"
+                      :not {:facility "auth,authpriv,cron,daemon,mail,news"}}}
+   :f_emerg { :level "emerg" }
+   :f_xconsole {:or {:facility "daemon,mail"
+                     :level "debug,info,notice,warn"
+                     :and {:facility "news"
+                           :level "crit,err,notice"}}}})
+
+(def default-logs
+  [{:source :s_all
+    :filter :f_auth
+    :destination :df_auth}
+   {:source :s_all
+    :filter :f_syslog
+    :destination :df_syslog}
+   {:source :s_all
+    :filter :f_daemon
+    :destination :df_daemon}
+   {:source :s_all
+    :filter :f_kern
+    :destination :df_kern}
+   {:source :s_all
+    :filter :f_lpr
+    :destination :df_lpr}
+   {:source :s_all
+    :filter :f_mail
+    :destination :df_mail}
+   {:source :s_all
+    :filter :f_user
+    :destination :df_user}
+   {:source :s_all
+    :filter :f_uucp
+    :destination :df_uucp}
+   {:source :s_all
+    :filter [:f_mail :f_at_least_info]
+    :destination :df_facility_dot_info}
+   {:source :s_all
+    :filter [:f_mail :f_at_least_warn]
+    :destination :df_facility_dot_warn}
+   {:source :s_all
+    :filter [:f_mail :f_at_least_err]
+    :destination :df_facility_dot_err}
+   {:source :s_all
+    :filter [:f_news :f_at_least_crit]
+    :destination :df_news_dot_crit}
+   {:source :s_all
+    :filter [:f_news :f_at_least_err]
+    :destination :df_news_dot_err}
+   {:source :s_all
+    :filter [:f_news :f_at_least_notice]
+    :destination :df_news_dot_notice}
+   {:source :s_all
+    :filter :f_debug
+    :destination :df_debug}
+   {:source :s_all
+    :filter :f_messages
+    :destination :df_messages}
+   {:source :s_all
+    :filter :f_emerg
+    :destination :du_all}
+   {:source :s_all
+    :filter :f_xconsole
+    :destination :dp_xconsole}])
+
+;; :key value -> key(value)
+;; :key { :p1 v1 :p2 v2 } -> key(p1(v1) p2(v2))
+;; :key { :value v :p1 v1 :p2 v2 } -> key(v p1(v1) p2(v2))
+
+(defmulti property-fmt
+  (fn [& [value & options]]
+    (cond
+     (map? value) :map
+     :else :default)))
+
+(defmulti value-fmt
+  (fn [value key]
+    (cond
+     (map? value) :map
+     (and (instance? Boolean value) value) :true
+     (instance? clojure.lang.Named value) :named
+     :else :default)))
+
+(defmethod property-fmt :map
+  ([value] (property-fmt value ";\n"))
+  ([value seperator]
+     (string/join
+      ""
+      (map
+       #(cond
+         (#{:not} (first %)) (str (name (first %)) " "
+                                  (property-fmt (second %) seperator))
+         (#{:and :or} (first %)) (str "("
+                                      (string/trim
+                                       (string/join
+                                        (str " " (name (first %)) " ")
+                                        (map (fn [x]
+                                               (property-fmt
+                                                (apply hash-map x) " "))
+                                             (second %))))
+                                      ")" seperator)
+         (vector? (second %)) (string/join
+                               ""
+                               (map (fn [x]
+                                      (property-fmt { (first %) x} seperator))
+                                    (second %)))
+         :else (str (name (first %)) "("
+                    (string/trim
+                     (str
+                      (value-fmt (second %) (first %))
+                      " "
+                      (property-fmt (second %) " ")))
+                    ")" seperator))
+       (dissoc value :value)))))
+
+(defmethod property-fmt :default
+  [value & _]
+  nil)
+
+(defn format-value [value key]
+  (if (#{:file :unix-stream :bad_hostname :template :log_prefix} key)
+    (pr-str value)
+    (str value)))
+
+(defmethod value-fmt :default
+  [value key]
+  (format-value value key))
+
+(defmethod value-fmt :true
+  [value key]
+  nil)
+
+(defmethod value-fmt :named
+  [value key]
+  (format-value (name value) key))
+
+(defmethod value-fmt :map
+  [value key]
+  (format-value (:value value) key))
+
+
+
+(defn configure-block
+  "Define a syslog-ng configuration block"
+  [block-type properties]
+  (format
+   "%s {\n%s\n};\n"
+   block-type
+   (string/trim (property-fmt properties))))
+
+(defn config*
+  [& {:keys [options sources destinations filters logs]
+      :or {options default-options
+           sources default-sources
+           destinations default-destinations
+           filters default-filters
+           logs default-logs}
+      :as args}]
+  (remote-file/remote-file*
+   "/etc/syslog-ng/syslog-ng.conf"
+   :content (str
+             (configure-block "options" options)
+             (string/join
+              \newline
+              (map #(configure-block
+                     (str "source " (name (first %))) (second %))
+                   sources))
+             (string/join
+              \newline
+              (map #(configure-block
+                     (str "destination " (name (first %))) (second %))
+                   destinations))
+             (string/join
+              \newline
+              (map #(configure-block
+                     (str "filter " (name (first %))) (second %))
+                   filters))
+             (string/join
+              \newline
+              (map #(configure-block "log" %) logs)))))
+
+(resource/defresource config
+  config* [& options])
+
+(defn install
+  "Install from package. keys correspond to sections of the syslog-ng.conf file"
+  [& {:keys [options sources destinations filters logs]
+      :as args}]
+  (package/package "syslog-ng")
+  (when-let [group (:group options)]
+    (user/group group :system true))
+  (when-let [dir_group (:dir_group options)]
+    (when (not (= dir_group (:group options)))
+      (user/group dir_group :system true)))
+  (apply
+   config
+   (apply concat
+          (select-keys args [:options :sources :destinations :filters :logs]))))
---
+++ test/pallet/crate/syslog_ng_test.clj
@@ -0,0 +1,40 @@
+(ns pallet.crate.syslog-ng-test
+  (:use pallet.crate.syslog-ng)
+
+  (:use clojure.test
+        pallet.test-utils))
+
+(deftest property-fmt-test
+  (testing "single property"
+    (is (= "group(logs);\n"
+           (property-fmt {:group 'logs}))))
+  (testing "multiple properties"
+    (is (= "group(logs);\ndir_group(logs);\n"
+           (property-fmt {:group 'logs :dir_group 'logs}))))
+  (testing "string property"
+    (is (= "bad_hostname(\"gconfd\");\n"
+           (property-fmt {:bad_hostname "gconfd"}))))
+  (testing "sub property"
+    (is (= "tcp(ip(0.0.0.0) port(5000) max-connections(300));\n"
+           (property-fmt
+            {:tcp {:ip "0.0.0.0" :port 5000 :max-connections 300}}))))
+  (testing "logic ops"
+    (is (= "(facility(daemon,mail)  or level(debug,info,notice,warn)  or (facility(news)  and level(crit,err,notice)));\n"
+           (property-fmt
+            {:or {:facility "daemon,mail"
+                  :level "debug,info,notice,warn"
+                  :and {:facility "news"
+                        :level "crit,err,notice"}}}))))
+  (testing "vector"
+    (is (= "filter(f1);\nfilter(f2);\n"
+           (property-fmt
+            {:filter [:f1 :f2]}))))
+  (testing "some examples"
+    (is (= "unix-stream(\"/dev/log\");\nudp();\nfile(\"f\" template(\"t\") template_escape(no));\n"
+           (property-fmt
+            {:unix-stream "/dev/log"
+             :udp true
+             :file {:value "f" :template "t" :template_escape "no"}})))))
+
+(deftest configure-block-test
+  (is (= "a {\nb(c);\n};\n" (configure-block "a" {:b "c"}))))
