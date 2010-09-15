66f356175ad3d02ee12468abfe813403bf2c333e
Hugo Duncan <hugo_duncan@yahoo.com>
2010-09-15 00:16:24 -0400

    remove defresource from crontab crate

--- src/pallet/crate/crontab.clj
+++ src/pallet/crate/crontab.clj
@@ -5,43 +5,49 @@
    [pallet.stevedore :as stevedore]
    [pallet.utils :as utils]
    [pallet.resource :as resource]
+   [pallet.resource.exec-script :as exec-script]
    [pallet.resource.file :as file]
    [pallet.resource.remote-file :as remote-file]
-   [pallet.resource.user :as user]))
+   [pallet.resource.user :as user])
+  (:use pallet.thread-expr))
 
 (def system-cron-dir "/etc/cron.d")
 
-(resource/defresource crontab
+(defn crontab
   "Manage a user's crontab file. Valid actions are :create or :remove.
    Options are as for remote-file."
-  (crontab*
-   [request user & {:keys [action] :or {action :create} :as options}]
-   (let [in-file (str (stevedore/script (user/user-home user)) "/crontab.in")]
-     (case action
-       :create (stevedore/do-script
+  [request user & {:keys [action] :or {action :create} :as options}]
+  (let [in-file (str (stevedore/script (user/user-home user)) "/crontab.in")]
+    (case action
+      :create (->
+               request
+               (apply->
+                remote-file/remote-file
+                in-file :owner user :mode "0600"
                 (apply
-                 remote-file/remote-file*
-                 request in-file :owner user :mode "0600"
+                 concat
+                 (select-keys options remote-file/content-options)))
+               (exec-script/exec-checked-script
+                "Load crontab"
+                ("crontab" -u ~user ~in-file)))
+      :remove (->
+               request
+               (file/file in-file :action :delete)
+               (exec-script/exec-checked-script
+                "Remove crontab"
+                ("crontab" -u ~user -r))))))
+
+(defn system-crontab
+  "Manage a system crontab file. Valid actions are :create or :remove.
+Options are as for remote-file."
+  [request name & options]
+  (let [options (apply hash-map options)
+        cron-file (str system-cron-dir "/" name)]
+    (condp = (get options :action :create)
+        :create (apply
+                 remote-file/remote-file request cron-file
+                 :owner "root" :group "root" :mode "0644"
                  (apply
                   concat
                   (select-keys options remote-file/content-options)))
-                (stevedore/script ("crontab" -u ~user ~in-file)))
-       :remove (stevedore/do-script
-                (file/file* request in-file :action :delete)
-                (stevedore/script ("crontab" -u ~user -r)))))))
-
-(resource/defresource system-crontab
-  "Manage a system crontab file. Valid actions are :create or :remove.
-Options are as for remote-file."
-  (system-crontab*
-   [request name & options]
-   (let [options (apply hash-map options)
-         cron-file (str system-cron-dir "/" name)]
-     (condp = (get options :action :create)
-         :create (apply
-                  remote-file/remote-file* request cron-file
-                  :owner "root" :group "root" :mode "0644"
-                  (apply
-                   concat
-                   (select-keys options remote-file/content-options)))
-         :remove (file/file* request cron-file :action :delete)))))
+        :remove (file/file request cron-file :action :delete))))
