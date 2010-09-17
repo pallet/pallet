6028be8f11b2bb70749b36bdfb9bc34784050d74
Hugo Duncan <hugo_duncan@yahoo.com>
2010-09-17 09:51:38 -0400

    switch test to use build-resources

--- test/pallet/crate/rubygems_test.clj
+++ test/pallet/crate/rubygems_test.clj
@@ -3,34 +3,43 @@
   (:require
    [pallet.resource :as resource]
    [pallet.resource.remote-file :as remote-file]
+   [pallet.resource.exec-script :as exec-script]
    [pallet.stevedore :as stevedore])
   (:use
    clojure.test
    pallet.test-utils
    [pallet.resource.package :only [package package-manager]]
-   [pallet.resource :only [build-resources]]
-   [pallet.stevedore :only [script]]))
+   [pallet.resource :only [build-resources]]))
 
 (deftest gem-script-test
   (is (= "gem install  fred"
-         (script (gem "install" "fred")))))
+         (stevedore/script (gem install fred)))))
 
 (deftest gem-test
-  (is (= (stevedore/checked-script
-          "Install gem fred"
-          (gem "install" "fred"))
+  (is (= (first
+          (resource/build-resources
+           []
+           (exec-script/exec-checked-script
+             "Install gem fred"
+             (gem install fred))))
          (first (build-resources [] (gem "fred"))))))
 
 (deftest gem-source-test
-  (is (= "if ! gem sources --list | grep http://rubygems.org; then gem sources --add http://rubygems.org;fi\n"
+  (is (= (first
+          (resource/build-resources
+           []
+           (exec-script/exec-script
+            "if ! gem sources --list | grep http://rubygems.org; then gem sources --add http://rubygems.org;fi\n")))
          (first (build-resources [] (gem-source "http://rubygems.org"))))))
 
 (deftest gemrc-test
-  (is (= (remote-file/remote-file*
-          {}
-          "$(getent passwd fred | cut -d: -f6)/.gemrc"
-          :content "\"gem\":\"--no-rdoc --no-ri\""
-          :owner "fred")
+  (is (= (first
+          (resource/build-resources
+           []
+           (remote-file/remote-file
+            "$(getent passwd fred | cut -d: -f6)/.gemrc"
+            :content "\"gem\":\"--no-rdoc --no-ri\""
+            :owner "fred")))
          (first
           (build-resources [] (gemrc {:gem "--no-rdoc --no-ri"} "fred"))))))
 
