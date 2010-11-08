fc2700a8e659b372f30cbd771fd8693e2f5bbb50
Hugo Duncan <hugo_duncan@yahoo.com>
2010-11-08 12:26:10 -0500

    Added reading of ~/.pallet/config.clj for settings

--- src/pallet/main_invoker.clj
+++ src/pallet/main_invoker.clj
@@ -4,6 +4,8 @@
    the world."
   (:require
    [clojure.contrib.logging :as logging]
+   [clojure.java.io :as java-io]
+   [clojure.walk :as walk]
    [pallet.compute :as compute]
    [pallet.blobstore :as blobstore]
    [pallet.utils :as utils]
@@ -25,55 +27,42 @@
      (format "public-key-path %s %s" public-key-path
              (.canRead (java.io.File. public-key-path))))))
 
-(defn compute-service-from-project
-  [project profiles]
-  (when-let [pallet-options (:pallet project)]
+(defn compute-service-properties
+  [config profiles]
+  (when config
     (let [provider (first profiles)
-          default-provider (map pallet-options
-                                [:provider :identity :credential])
-          providers (:providers pallet-options)]
+          default-provider (map config [:provider :identity :credential])
+          providers (:providers config)]
       (cond
-       (every? identity default-provider) (compute/compute-service
-                                           (:provider pallet-options)
-                                           :identity (:identity pallet-options)
-                                           :credential (:credential
-                                                        pallet-options))
-
-       (map? providers) (if-let [creds (or
-                                        (and provider
-                                             (providers provider)
-                                             [provider (providers provider)])
-                                        (-> providers first))]
-                          (compute/compute-service
-                           (first creds)
-                           :identity (:identity (second creds))
-                           :credential (:credential (second creds))))
+       (every? identity default-provider) {:provider (:provider config)
+                                           :identity (:identity config)
+                                           :credential (:credential config)
+                                           :blobstore (:blobstore config)}
+       (map? providers) (or
+                         (and provider (or
+                                        (providers (keyword provider))
+                                        (providers provider)))
+                         (and (not provider) ; use default if no profile
+                                             ; requested
+                              (first providers)
+                              (-> providers first val)))
        :else nil))))
 
-(defn blobstore-from-project
-  [project profiles]
-  (when-let [pallet-options (-> project :pallet :blobstore)]
-    (let [provider (first profiles)
-          default-provider (map pallet-options
-                                [:provider :identity :credential])
-          providers (:providers pallet-options)]
-      (cond
-       (every? identity default-provider) (blobstore/service
-                                           (:provider pallet-options)
-                                           :identity (:identity pallet-options)
-                                           :credential (:credential
-                                                        pallet-options))
+(defn compute-service-from-config
+  [config profiles]
+  (let [{:keys [provider identity credential]} (compute-service-properties
+                                                config profiles)]
+    (when provider
+      (compute/compute-service
+       provider :identity identity :credential credential))))
 
-       (map? providers) (if-let [creds (or
-                                        (and provider
-                                             (providers provider)
-                                             [provider (providers provider)])
-                                        (-> providers first))]
-                          (compute/compute-service
-                           (first creds)
-                           :identity (:identity (second creds))
-                           :credential (:credential (second creds))))
-       :else nil))))
+(defn blobstore-from-config
+  [config profiles]
+  (let [config (compute-service-properties config profiles)
+        {:keys [provider identity credential]} (merge config
+                                                      (:blobstore config))]
+    (when provider
+      (blobstore/service provider identity credential))))
 
 (defn find-compute-service
   "Look for a compute service in the following sequence:
@@ -82,12 +71,12 @@
      check pallet.config/service var.
    This sequence allows you to specify an overridable default in
    pallet.config/service."
-  [project profiles]
+  [defaults project profiles]
   (or
-   (compute-service-from-project project profiles)
+   (compute-service-from-config (:pallet project) profiles)
+   (compute-service-from-config defaults profiles)
    (compute/compute-service-from-property)
-   (apply compute/compute-service-from-settings profiles)
-   (compute/compute-service-from-config)))
+   (apply compute/compute-service-from-settings profiles)))
 
 
 (defn find-blobstore
@@ -97,23 +86,71 @@
      check pallet.config/service var.
    This sequence allows you to specify an overridable default in
    pallet.config/service."
-  [project profiles]
+  [defaults project profiles]
   (or
-   (blobstore-from-project project profiles)
+   (blobstore-from-config (:pallet project) profiles)
+   (blobstore-from-config defaults profiles)
    (apply blobstore/blobstore-from-settings profiles)))
+
+
+(def ^{:private true} config nil)
+
+(defn- unquote-vals [args]
+  (walk/walk
+   (fn [item]
+     (cond (and (seq? item) (= `unquote (first item))) (second item)
+           ;; needed if we want fn literals to be usable by eval-in-project
+           (and (seq? item) (= 'fn (first item))) (list 'quote item)
+           (symbol? item) (list 'quote item)
+           :else (unquote-vals item)))
+   identity
+   args))
+
+(defmacro defpallet
+  [& {:keys [provider identity credential providers admin-user]
+      :as config-options}]
+  `(let [m# (zipmap
+             ~(cons 'list (keys config-options))
+             ~(cons 'list (unquote-vals (vals config-options))))]
+    (alter-var-root
+     #'config
+     (fn [_#] m#))))
+
+(defn- read-config
+  [file]
+  (try
+    (load-file file)
+    config
+    (catch java.io.FileNotFoundException _)))
+
+(defn- home-dir
+  "Returns full path to Pallet home dir ($PALLET_HOME or $HOME/.pallet)"
+  []
+  (.getAbsolutePath
+   (doto (if-let [pallet-home (System/getenv "PALLET_HOME")]
+           (java.io.File. pallet-home)
+           (java.io.File. (System/getProperty "user.home") ".pallet"))
+     .mkdirs)))
+
+(defn pallet-config
+  []
+  (read-config (.getAbsolutePath (java-io/file (home-dir) "config.clj"))))
 
 (defn invoke
   [service user key profiles task params project-options]
   (utils/admin-user-from-config)
   (log-info)
-  (let [compute (if service
+  (let [default-config (pallet-config)
+        compute (if service
                   (compute/compute-service
                    service :identity user :credential key)
-                  (find-compute-service project-options profiles))
+                  (find-compute-service
+                   default-config project-options profiles))
         blobstore (if service
                     (blobstore/service
                      service :identity user :credential key)
-                    (find-blobstore project-options profiles))]
+                    (find-blobstore
+                     default-config project-options profiles))]
     (if compute
       (do
         (logging/debug (format "Running as      %s@%s" user service))
