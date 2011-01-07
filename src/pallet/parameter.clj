1804e93aef7cc0c100a3d752305ff3c479f2d4cd
Hugo Duncan <hugo@hugoduncan.org>
2011-01-07 10:13:00 -0500

    Remove spurious parameter/get-for-service definition

--- src/pallet/parameter.clj
+++ src/pallet/parameter.clj
@@ -80,14 +80,6 @@
   ([request keys default]
      (get-for request (concat [:service] keys) default)))
 
-(defn get-for-service
-  ([request keys]
-     (get-for
-      request (concat [:service] keys)))
-  ([request keys default]
-     (get-for
-      request (concat [:service] keys) default)))
-
 (defn- assoc-for-prefix
   "Set the values in a map at the paths specified with prefix prepended to each
    path.
