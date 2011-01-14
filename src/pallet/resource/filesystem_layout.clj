cf33b9dcd989ad8598622790bb9ccac22117abe8
Hugo Duncan <hugo@hugoduncan.org>
2011-01-13 23:03:36 -0500

    Add pkg- prefixed path functions, for locations of files managed by the package manager Package managers such as brew do not install files in the base system locations

--- src/pallet/resource/filesystem_layout.clj
+++ src/pallet/resource/filesystem_layout.clj
@@ -1,12 +1,14 @@
 (ns pallet.resource.filesystem-layout
-  "Functions to return distribution specific paths"
+  "Functions to return distribution specific paths.
+
+   These script functions are meant to help build distribution agnostic crates."
   (:require
    [pallet.script :as script]
    [pallet.stevedore :as stevedore]))
 
 (script/defscript etc-default [])
-(stevedore/defimpl etc-default [#{:ubuntu :debian :jeos :fedora}] []
-  "/etc/default")
+(stevedore/defimpl etc-default [#{:ubuntu :debian :jeos :fedora :darwin}] []
+  "/etc/defaults")
 (stevedore/defimpl etc-default [#{:centos :rhel :amzn-linux}] []
   "/etc/sysconfig")
 
@@ -29,3 +31,27 @@
 (script/defscript etc-init [])
 (stevedore/defimpl etc-init :default []
   "/etc/init.d")
+
+;;; Some of the packagers, like brew, are "add-ons" in the sense that they are
+;;; outside of the base system.  These paths refer to locations of packager
+;;; installed files.
+
+(script/defscript pkg-etc-default [])
+(stevedore/defimpl pkg-etc-default :default [] (etc-default))
+(stevedore/defimpl etc-default [:brew] [] "/usr/local/etc/default")
+
+(script/defscript pkg-log-root [])
+(stevedore/defimpl pkg-log-root :default [] (log-root))
+(stevedore/defimpl pkg-log-root [:brew] [] "/usr/local/var/log")
+
+(script/defscript pkg-pid-root [])
+(stevedore/defimpl pkg-pid-root :default [] (pid-root))
+(stevedore/defimpl pkg-pid-root [:brew] [] "/usr/local/var/run")
+
+(script/defscript pkg-config-root [])
+(stevedore/defimpl pkg-config-root :default [] (config-root))
+(stevedore/defimpl pkg-config-root [:brew] [] "/usr/local/etc")
+
+(script/defscript pkg-sbin [])
+(stevedore/defimpl pkg-sbin :default [] "/sbin")
+(stevedore/defimpl pkg-sbin [:brew] [] "/usr/local/sbin")
