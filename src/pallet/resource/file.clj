cee1393c7759c7110a619b63d8e2256a32917b32
Hugo Duncan <hugo@hugoduncan.org>
2011-02-09 11:00:22 -0500

    Add :proxy option to pallet.resource.file/download-file

--- src/pallet/resource/file.clj
+++ src/pallet/resource/file.clj
@@ -95,18 +95,26 @@
       (format "-e \"%s%s\"" (when restriction (str restriction " ")) expr-map))
    ~file))
 
-(script/defscript download-file [url path])
+(script/defscript download-file [url path & {:keys [proxy]}])
 
-(stevedore/defimpl download-file :default [url path]
+(stevedore/defimpl download-file :default [url path & {:keys [proxy]}]
   (if (test @(which curl))
     ("curl" "-o" (quoted ~path)
      --retry 5 --silent --show-error --fail --location
+     ~(if proxy
+        (let [url (java.net.URL. proxy)]
+          (format "--proxy %s:%s" (.getHost url) (.getPort url)))
+        "")
      (quoted ~url))
     (if (test @(which wget))
-      ("wget" "-O" (quoted ~path) --tries 5 --no-verbose (quoted ~url))
-     (do
-         (println "No download utility available")
-         (exit 1)))))
+      ("wget" "-O" (quoted ~path) --tries 5 --no-verbose
+       ~(if proxy
+          (format "-e \"http_proxy = %s\" -e \"ftp_proxy = %s\"" proxy proxy)
+          "")
+       (quoted ~url))
+      (do
+        (println "No download utility available")
+        (exit 1)))))
 
 (script/defscript download-request [path request])
 (stevedore/defimpl download-request :default [path request]
