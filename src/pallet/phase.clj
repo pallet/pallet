8b2378f6fa30ef115ef4dd527b331d949e1d05b8
Sam Ritchie <sritchie09@gmail.com>
2011-03-31 22:58:02 -0400

    Added macro-writing macro that allows for custom definition of functions similar to `phase-fn`, with the added benefit that `defthreadfn` allows for any number of checkers to be placed after each threaded argument.
    For example,
    
        (defthreadfn ultrasafe-phase-fn
            "docstring!"
             watchdog-func-1 watchdog-func-2 watchdog-func-3)
    
    Will define a macro that can be called like this:
    
        (ultrasafe-phase-fn [x y z]
            (phase1 x y)
            (phase2 x y z))
    
    Which will (recursively) expand into
    
        (fn [arg x y z]
          (--> arg
               (phase1 x y)
               (watchdog-func-1 "(phase1 x y)")
               (watchdog-func-2 "(phase1 x y)")
               (watchdog-func-3 "(phase1 x y)")
               (phase2 x y z)
               (watchdog-func-1 "(phase2 x y z)")
               (watchdog-func-2 "(phase2 x y z)")
               (watchdog-func-3 "(phase2 x y z)")))
    
    `-->`, from a prior commit in the pull request, allows for the use of
    `for`, `when`, let` and `expose-request-as` within the body of the
    function. What's nice about this construction is that
    
        (defthreadfn unchecked-phase-fn)
    
    When called, expands to
    
        (fn [arg x y z]
          (--> arg
               (phase1 x y)
               (phase2 x y z)
    
    Which simply gives access to `-->`, with no checking.
    
    Currently, defthreadfn accepts attr-maps and docstrings, but doesn't
    format its attr-map well. I'll look at this for the next commit --
    this macro is debugged, so I figured I'd submit it for your initial
    comments.

--- src/pallet/phase.clj
+++ src/pallet/phase.clj
@@ -2,7 +2,8 @@
   "A phase is a function of a single `session` argument, that contains
    calls to crate functions or actions. A phase has an implicitly
    defined pre and post phase."
-  (:use [pallet.thread-expr :only (-->)])
+  (:use [pallet.thread-expr :only (-->)]
+        [clojure.contrib.def :only (name-with-attributes)])
   (:require
    [clojure.contrib.condition :as condition]))
 
@@ -48,41 +49,83 @@
 
 (defn check-session
   "Function that can check a session map to ensure it is a valid part of
-   phase definiton. It returns the session map.
+   phase definition. It returns the session map.
 
-   If this fails, then it is likely that you have an incorrect crate function,
-   which is failing to return its session map properly, or you have a non crate
-   function in the phase defintion."
-  ([session]
-     ;; we do not use a precondition in order to improve the error message
-     (when-not (and session (map? session))
-       (condition/raise
-        :type :invalid-session
-        :message
-        "Invalid session map in phase. Check for non crate functions,
-      improper crate functions, or problems in threading the session map
-      in your phase definition.
+   On failure, the function will print the phase through which the
+   session passed prior to crashing. It is like that this phase
+   introduced the fault into the session; to aid in debugging, make
+   sure to use `phase-fn` and `def-phase-fn` to build phases."
+  [session form]
+  ;; we do not use a precondition in order to improve the error message
+  (when-not (and session (map? session))
+    (condition/raise
+     :type :invalid-session
+     :message
+     (str
+      "Invalid session map in phase.\n"
+      (format "session is %s\n" session)
+      (format "Problem probably caused by subphase:\n  %s\n" form)
+      "Check for non crate functions, improper crate functions, or
+      problems in threading the session map in your phase
+      definition. A crate function is a function that takes a session
+      map and other arguments, and returns a modified session
+      map. Calls to crate functions are often wrapped in a threading
+      macro, -> or pallet.phase/phase-fn, to simplify chaining of the
+      session map argument.")))
+  session)
+(defthreadfn mapped-thread-fn
+  (fn [arg sub-expr]
+    (if (map? arg)
+      arg
+      (condition/raise
+       :type :invalid-session
+       :message
+       (format "Only maps are allowed, and %s is most definitely not a map."
+               sub-expr)))))
 
-      A crate function is a function that takes a session map and other
-      arguments, and returns a modified session map. Calls to crate functions
-      are often wrapped in a threading macro, -> or pallet.phase/phase-fn,
-      to simplify chaining of the session map argument."))
-     session)
-  ([session form]
-     ;; we do not use a precondition in order to improve the error message
-     (when-not (and session (map? session))
-       (condition/raise
-        :type :invalid-session
-        :message
-        (format
-         (str
-          "Invalid session map in phase session.\n"
-          "`session` is %s\n"
-          "Problem probably caused in:\n  %s ")
-         session form)))
-     session))
+(defmacro defthreadfn
+  "Binds a var to a particular class of anonymous functions that
+  accept a vector of arguments and a number of subexpressions. When
+  calling the produced functions, the caller must supply a threading
+  parameter as the first argument. This parameter will be threaded
+  through the resulting forms; the extra parameters made available in
+  the argument vector will be available to all subexpressions.
 
-(defmacro phase-fn
+  In addition to the argument vector, `defthreadfn` accepts any number
+  of watchdog functions, each of which are inserted into the thread
+  after each subexpression invocation. These watchdog functions are
+  required to accept 2 arguments -- the threaded argument, and a
+  string representation of the previous subexpression. For example,
+
+    (defthreadfn mapped-thread-fn
+      (fn [arg sub-expr]
+        (if (map? arg)
+          arg
+          (condition/raise
+           :type :invalid-session
+           :message
+           (format \"Only maps are allowed, and %s is most definitely
+                   not a map.\" sub-expr)))))
+
+  Returns a `thread-fn` that makes sure the threaded argument remains
+  a map, on every step of its journey.
+
+  For a more specific example if `defthreadfn`, see
+  `pallet.phase/phase-fn`."
+  [macro-name & rest]
+  (let [[macro-name checkers] (name-with-attributes macro-name rest)]
+    `(defmacro ~macro-name
+       ([argvec#] (~macro-name argvec# identity))
+       ([argvec# subphase# & left#]
+          `(fn [~'session# ~@argvec#]
+             (--> ~'session#
+                  ~subphase#
+                  ~@(for [func# '~checkers]
+                      `(~func# (str '~subphase#)))
+                  ~@(when left#
+                      [`((~'~macro-name ~argvec# ~@left#) ~@argvec#)])))))))
+
+(defthreadfn phase-fn
   "Composes a phase function from a sequence of phases by threading an
  implicit phase session parameter through each. Each phase will have
  access to the parameters passed in through `phase-fn`'s argument
@@ -99,13 +142,20 @@
          (file filename)
          (file \"/other-file\")))
   
-   with an added safety call to `check-session` prior to each phase
-   invocation."
-  ([argvec] `(phase-fn ~argvec identity))
-  ([argvec subphase & left]
-     `(fn [session# ~@argvec]
-        (--> session#
-            ~subphase
-            (check-session (str "The session passed out of" '~subphase))
-            ~@(when left
-                [`((phase-fn ~argvec ~@left) ~@argvec)])))))
+   with a number of verifications on the session map performed after
+   each phase invocation."
+  check-session)
+
+(defthreadfn unchecked-phase-fn
+  "Unchecked version of `phase-fn`.
+
+   The following two forms are equivalent:
+
+   (unchecked-phase-fn [x y]
+       (+ x)
+       (+ y))
+
+   (fn [session x y]
+       (-> session
+           (+ x)
+           (+ y)))")
