pallet-attic/pallet-crates

Allow specification of a :service parameter path for ssh-key/record-public-key

ssh-key/src/pallet/crate/ssh_key.clj
@@ -111,7 +111,8 @@
      (file/file (str path ".pub") :owner user :mode "0644"))))
 
 (defn record-public-key
-  [request user & {:keys [filename type]
+  "Record a public key"
+  [request user & {:keys [filename type parameter-path]
                    :or {type "rsa"} :as options}]
   (let [filename (or filename (ssh-default-filenames type))
         path (str (user-ssh-dir user) filename ".pub")]
@@ -122,9 +123,15 @@
          (resource/as-local-resource
           request
           (fn [request]
-            (parameter/assoc-for-target
-             request [:user (keyword user) (keyword filename)]
-             (slurp local-path)))))
+            (let [pub-key (slurp local-path)]
+              (if-not (string/blank? pub-key)
+                (if parameter-path
+                  (parameter/update-for-service
+                   request parameter-path
+                   (fn [keys] (conj (or keys #{}) pub-key)))
+                  (parameter/assoc-for-target
+                   request [:user (keyword user) (keyword filename)] pub-key))
+                request)))))
        path))))
 
 #_

pallet/pallet

src/pallet/crate/ssh_key.clj
@@ -109,7 +109,8 @@
      (file/file (str path ".pub") :owner user :mode "0644"))))
 
 (defn record-public-key
-  [session user & {:keys [filename type]
+  "Record a public key"
+  [session user & {:keys [filename type parameter-path]
                    :or {type "rsa"} :as options}]
   (let [filename (or filename (ssh-default-filenames type))
         path (str (user-ssh-dir user) filename ".pub")]
@@ -119,9 +120,15 @@
        (fn [session local-path]
          (action/as-clj-action
           (fn [session]
-            (parameter/assoc-for-target
-             session [:user (keyword user) (keyword filename)]
-             (slurp local-path)))
+            (let [pub-key (slurp local-path)]
+              (if-not (string/blank? pub-key)
+                (if parameter-path
+                  (parameter/update-for-service
+                   session parameter-path
+                   (fn [keys] (conj (or keys #{}) pub-key)))
+                  (parameter/assoc-for-target
+                   session [:user (keyword user) (keyword filename)] pub-key))
+                session)))
           [session]))
        path))))
 