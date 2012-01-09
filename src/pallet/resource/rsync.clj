67ecd836729ee89e9c7bd10d6d6df8b778461051
Hugo Duncan <hugo@hugoduncan.org>
2012-01-09 08:08:18 -0500

    Remove pallet.resource.*

--- src/pallet/resource/rsync.clj
+++
@@ -1,9 +0,0 @@
-(ns pallet.resource.rsync
-  "Compatability namespace"
-  (:require
-   pallet.action.rsync
-   [pallet.common.deprecate :as deprecate]))
-
-(deprecate/forward-fns
- pallet.action.rsync
- rsync rsync-directory)
