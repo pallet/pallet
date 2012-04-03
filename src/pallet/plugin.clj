29cd13548f9bd882fb34f814d377c707d3880529
Hugo Duncan <hugo@hugoduncan.org>
2012-04-03 10:02:49 -0400

    Refactor plugin loading
    Make plugins load based on a function named by the plugins namespace name, as in lein.

--- src/pallet/plugin.clj
+++ src/pallet/plugin.clj
@@ -1,10 +1,12 @@
 (ns pallet.plugin
-  (:use [chiba.plugin :only [plugins]]))
+  (:use
+   [chiba.plugin :only [plugins]]
+   [pallet.monad :only [session-pipeline]]))
 
-(defn load-plugins
-  "Load pallet plugins"
-  []
-  (let [plugin-namespaces (plugins "pallet.plugin." #".*test.*")]
-    (doseq [plugin plugin-namespaces]
-      (require plugin))
-    plugin-namespaces))
+;; (defn load-plugins
+;;   "Load pallet plugins"
+;;   []
+;;   (let [plugin-namespaces (plugins "pallet.plugin." #".*test.*")]
+;;     (doseq [plugin plugin-namespaces]
+;;       (require plugin))
+;;     plugin-namespaces))
