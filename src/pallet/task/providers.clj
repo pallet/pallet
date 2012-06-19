2e8b009b56eb6d4a446a070ff4e19f5e87e7762a
Hugo Duncan <hugo@hugoduncan.org>
2012-06-19 10:48:27 -0400

    Add support for jclouds Providers API
    Jclouds 1.5 and greater has a Providers interface for discovering providers
    in place of the previous properties file.

--- src/pallet/task/providers.clj
+++ src/pallet/task/providers.clj
@@ -16,19 +16,32 @@
    (Class/forName provider)
    (catch java.lang.ClassNotFoundException e)))
 
+(defn providers-from-properties
+  []
+  (for [supported (sort #(compare (first %1) (first %2))
+                          (provider-properties))
+          :let [key (first supported)
+                name (.substring key 0 (.indexOf key "."))]]
+    [name (enabled? (second supported))]))
+
+(defmacro jclouds-providers-fn
+  []
+  (if (Class/forName "org.jclouds.providers.Providers")
+    `(defn jclouds-providers
+       []
+       (for [provider# (org.jclouds.providers.Providers/viewableAs
+                        org.jclouds.compute.ComputeServiceContext)]
+         [(.getName provider#) true]))
+    `(defn jclouds-providers [] (providers-from-properties))))
+
+(jclouds-providers-fn)
+
 (defn providers
   "Provide information on the supported and enabled providers."
   {:no-service-required true}
   [& _]
   (println "Pallet uses jcloud's providers.\n")
-  (doseq [supported (sort #(compare (first %1) (first %2))
-                          (provider-properties))
-          :let [key (first supported)
-                name (.substring key 0 (.indexOf key "."))]]
-    (println
-     (format
-      "\t%20s  %s"
-      name
-      (if (enabled? (second supported)) "Enabled" "Disabled"))))
+  (doseq [[name enabled] (jclouds-providers)]
+    (println (format "\t%38s  %s" name (if enabled "Enabled" "Disabled"))))
   (println "\nProviders can be enabled by adding a dependency on the jclouds ")
   (println "provider into your project.clj or pom.xml."))
