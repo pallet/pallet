78443a25dc903c42ecbf83ade4a0b917054545c9
Hugo Duncan <hugo@hugoduncan.org>
2012-06-20 12:32:22 -0400

    Update unit test for nodes-with-role

--- test/pallet/session_test.clj
+++ test/pallet/session_test.clj
@@ -7,15 +7,15 @@
 
 (deftest groups-with-role-test
   (is (= [:group1] (session/groups-with-role
-                     {:node-set #{(core/group-spec "group1" :roles :role1)
-                                  (core/group-spec "group2" :roles :role2)}}
+                     {:all-node-set #{(core/group-spec "group1" :roles :role1)
+                                      (core/group-spec "group2" :roles :role2)}}
                      :role1))))
 
 (deftest nodes-with-role-test
   (let [n1 (test-utils/make-node "group1")
         n2 (test-utils/make-node "group2")]
     (is (= [n1] (session/nodes-with-role
-                  {:node-set #{(core/group-spec "group1" :roles :role1)
-                               (core/group-spec "group2" :roles :role2)}
+                  {:all-node-set #{(core/group-spec "group1" :roles :role1)
+                                   (core/group-spec "group2" :roles :role2)}
                    :all-nodes [n1 n2]}
                   :role1)))))
