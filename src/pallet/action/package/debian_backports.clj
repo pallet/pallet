cfd1efecb69ec77adab6cc35abff5552dcc68ec7
Hugo Duncan <hugo@hugoduncan.org>
2012-06-20 12:25:48 -0400

    Split out debian-backports-repository

--- src/pallet/action/package/debian_backports.clj
+++ src/pallet/action/package/debian_backports.clj
@@ -1,19 +1,28 @@
 (ns pallet.action.package.debian-backports
   "Actions for working with the debian backports repository"
   (:require
-   [pallet.action.package :as package]
    [pallet.parameter :as parameter]
    [pallet.script.lib :as lib]
    [pallet.session :as session]
-   [pallet.stevedore :as stevedore]))
+   [pallet.stevedore :as stevedore])
+  (:use
+   [pallet.action.package :only [package-source]]
+   [pallet.utils :only [apply-map]]))
+
+(defn debian-backports-repository
+  "debian backport package repository"
+  []
+  {:name "debian-backports"
+   :aptitude {:url "http://backports.debian.org/debian-backports"
+              :release (str
+                        (stevedore/script (~lib/os-version-name)) "-backports")
+              :scopes ["main"]}
+   :apt {:url "http://backports.debian.org/debian-backports"
+         :release (str (stevedore/script (~lib/os-version-name)) "-backports")
+         :scopes ["main"]}})
 
 (defn add-debian-backports
   "Add debian backport package repository"
   [session]
-  (package/package-source
-   session
-   "debian-backports"
-   :aptitude {:url "http://backports.debian.org/debian-backports"
-              :release (str
-                        (stevedore/script (~lib/os-version-name)) "-backports")
-              :scopes ["main"]}))
+  (let [source (debian-backports-repository)]
+    (apply-map package-source session (:name source) source)))
