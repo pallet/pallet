7f7a9a7932fb36c7342a0bd78f82116227dbd980
Ryan Stradling <ryan.stradling@fiveoneninegames.com>
2012-08-01 15:28:56 -0400

    Added documentation about the fact that usermod and useradd with a password depends upon the expect package which currently is not installed by default

--- src/pallet/script/lib.clj
+++ src/pallet/script/lib.clj
@@ -425,6 +425,9 @@
 ;; smartos does not support useradd
 ;; with password.  passwd needs to be
 ;; called instead
+;; Please note for smartos modify user with a password option will rely
+;; upon expect being installed on the server.  Currently this is not done
+;; for you.
 (script/defimpl create-user [#{:smartos}]
   [username options]
    ("/usr/sbin/useradd"
@@ -483,6 +486,9 @@
         stevedore/map-to-arg-string)
    ~username))
 
+;; Please note for smartos modify user with a password option will rely
+;; upon expect being installed on the server.  Currently this is not done
+;; for you.
 (script/defimpl modify-user [#{:smartos}]
   [username options]
    ("/usr/sbin/usermod"
