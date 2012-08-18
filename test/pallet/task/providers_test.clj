e04cf34b32d9bfec6fdc96b3ff79d6726ed02d63
Hugo Duncan <hugo@hugoduncan.org>
2012-08-18 10:25:05 -0400

    Simplify providers task
    The task was not robust across pallet and jclouds provider versions

--- test/pallet/task/providers_test.clj
+++ test/pallet/task/providers_test.clj
@@ -1,19 +1,9 @@
 (ns pallet.task.providers-test
-  (:use pallet.task.providers)
-  (:require [pallet.core :as core])
   (:use
    clojure.test
+   pallet.task.providers
    pallet.test-utils))
 
-
-(with-private-vars [pallet.task.providers
-                    [provider-properties enabled?]]
-
-  (deftest provider-properties-test
-    (is (map? (provider-properties)))
-    (is (every? string? (keys (provider-properties))))
-    (is (every? string? (vals (provider-properties)))))
-
-  (deftest enabled?-test
-    (is (enabled? "java.lang.String"))
-    (is (not (enabled? "java.lang.StringThatDoesntExist")))))
+(deftest providers-test
+  []
+  (is (with-out-str (providers nil))))
