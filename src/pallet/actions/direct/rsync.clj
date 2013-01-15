0705b49eddd48a76b7fea123b88de4c1f6bf6a32
Hugo Duncan <hugo@hugoduncan.org>
2013-01-15 00:00:55 -0500

    Make rsync action take options
    Allows passing a map of option flags.  By default rsync is now less verbose,
    with no -P flag.

--- src/pallet/actions/direct/rsync.clj
+++ src/pallet/actions/direct/rsync.clj
@@ -12,16 +12,22 @@
    [pallet.node :only [primary-ip]]))
 
 (def ^{:private true}
-  cmd "/usr/bin/rsync -e '%s' -rP --delete --copy-links -F -F %s %s@%s:%s")
+  cmd "/usr/bin/rsync -e '%s' -r --delete --copy-links -F -F%s %s %s@%s:%s")
 
 (implement-action rsync :direct
-  {:action-type :script :location :origin}
-  [session from to {:keys [port]}]
+                  {:action-type :script :location :origin}
+                  [session from to {:keys [port] :as options}]
   (logging/debugf "rsync %s to %s" from to)
-  (let [ssh (str "/usr/bin/ssh -o \"StrictHostKeyChecking no\" "
+  (let [extra-options (dissoc options :port)
+        ssh (str "/usr/bin/ssh -o \"StrictHostKeyChecking no\" "
                  (if port (format "-p %s" port)))
         cmd (format
-             cmd ssh from (:username (admin-user session))
+             cmd ssh
+             (if (seq extra-options)
+               (str " " (stevedore/map-to-arg-string extra-options))
+               "")
+
+             from (:username (admin-user session))
              (target-ip session) to)]
     [[{:language :bash}
       (stevedore/checked-commands (format "rsync %s to %s" from to) cmd)]
