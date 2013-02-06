ade5769682202f62f1ffa6ed87675206c1bdcbb2
Hugo Duncan <hugo@hugoduncan.org>
2013-02-06 12:23:00 -0500

    Add p.action/with-action-options to p.project.load
    Makes with-action-options available unqualified in the pallet.clj project file.

--- src/pallet/project/load.clj
+++ src/pallet/project/load.clj
@@ -2,6 +2,7 @@
   "Namespace for loading pallet project files.  Provides a default set of
   requires that are available to the pallet file."
   (:require
+   [pallet.action :refer [with-action-options]]
    [pallet.actions :refer :all :exclude [update-settings assoc-settings]]
    [pallet.api :refer :all]
    [pallet.crate :refer :all :exclude [compute-service]]
