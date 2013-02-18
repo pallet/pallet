b7fad3c98ff21a4805ea675f9831040f949b6256
Hugo Duncan <hugo@hugoduncan.org>
2013-02-17 23:31:31 -0500

    Add service-properties and image-user to node-list

--- src/pallet/compute/node_list.clj
+++ src/pallet/compute/node_list.clj
@@ -42,6 +42,8 @@
   (packager [node] (compute/packager-for-os os-family os-version))
   pallet.node.NodeHardware
   (hardware [node] hardware)
+  pallet.node.NodeImage
+  (image-user [node])
   pallet.node.NodeProxy
   (proxy [node] proxy))
 
@@ -120,7 +122,12 @@ support."
   (tag-node! [compute node tag-name value]
     (compute/tag-node! tag-provider node tag-name value))
   (node-taggable? [compute node]
-    (compute/node-taggable? tag-provider node)))
+    (compute/node-taggable? tag-provider node))
+  pallet.compute.ComputeServiceProperties
+  (service-properties [_]
+    {:provider :node-list
+     :nodes @node-list
+     :environment environment}))
 
 
 
