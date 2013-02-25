91080f4784e71f6fcf2c7c93ec90034e32e70712
Hugo Duncan <hugo@hugoduncan.org>
2013-02-25 16:02:04 -0500

    Update to fsmop 0.2.1
    Adds running? predicate and three-value returns for failed? and complete?

--- project.clj
+++ project.clj
@@ -24,7 +24,7 @@ unprecedented level of customization."
                  [org.cloudhoist/script-exec "0.2.0"]
                  [com.palletops/stevedore "0.8.0-beta.1"]
                  [enlive "1.0.1"]
-                 [pallet-fsmop "0.2.0"]
+                 [pallet-fsmop "0.2.1"]
                  [pallet-map-merge "0.1.0"]
                  [useful "0.8.6"]
                  [commons-codec "1.4"]]
