fda6b7d476a80489c28def825058db7d8844b08c
Hugo Duncan <hugo@hugoduncan.org>
2013-03-02 10:08:08 -0500

    Default local script to run without sudo
    When running local scripts, do not use sudo by default.  Actions can ask for
    sudo by using `:exec-prefix :sudo` in their implementation metadata.

--- src/pallet/local/execute.clj
+++ src/pallet/local/execute.clj
@@ -34,6 +34,15 @@
               :script-err (:err result)
               :server "localhost"})))
 
+
+(defn build-code
+  "Build the code with defaults for local execution."
+  [session action ^java.io.File tmpfile]
+  (script-builder/build-code
+   session
+   (assoc action :default-script-prefix :no-sudo)
+   (.getPath tmpfile)))
+
 (defn script-on-origin
   "Execute a script action on the origin"
   [session action action-type [options value]]
@@ -51,7 +60,7 @@
           (logging/warnf
            "script-on-origin: Could not chmod script file: %s"
            (:out result))))
-      (let [cmd (script-builder/build-code session action (.getPath tmpfile))
+      (let [cmd (build-code session action tmpfile)
             _ (logging/debugf "localhost %s" cmd)
             result (transport/exec
                     local-connection cmd
---
+++ test/pallet/local/execute_test.clj
@@ -0,0 +1,14 @@
+(ns pallet.local.execute-test
+  (:require
+   [clojure.test :refer [deftest is]]
+   [pallet.local.execute :refer :all]
+   [pallet.script :refer [with-script-context]]
+   [pallet.stevedore :refer [with-script-language]]))
+
+(deftest build-code-test
+  (is (= {:execv ["/usr/bin/env" "/bin/bash" "tmpf"]}
+         (build-code {} {} (java.io.File. "tmpf"))))
+  (is (= {:execv ["/usr/bin/sudo" "-n" "/usr/bin/env" "/bin/bash" "tmpf"]}
+         (with-script-language :pallet.stevedore.bash/bash
+           (with-script-context [:ubuntu]
+             (build-code {} {:script-prefix :sudo} (java.io.File. "tmpf")))))))
