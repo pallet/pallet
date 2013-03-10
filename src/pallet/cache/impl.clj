39be019782170d37761b75fc6818f98a4bf0b4d3
Hugo Duncan <hugo@hugoduncan.org>
2013-03-10 17:59:44 -0400

    Remove unused cache implementation

--- src/pallet/cache/impl.clj
+++
@@ -1,9 +0,0 @@
-(ns pallet.cache.impl
-  "An implementation namespace for pallet.cache")
-
-(defprotocol CacheProtocolImpl
-  "Cache implementation interface."
-  (lookup [cache e] [cache e default]
-    "Retrieve the value associated with `e` if it exists")
-  (has? [cache e]
-    "Checks if the cache contains a value associtaed with `e`"))
