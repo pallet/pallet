08f2e150a1d8b265202b8dd2d30b43f9745ef887
Pepijn de Vos <pepijndevos@gmail.com>
2013-03-14 18:42:46 +0100

    Write sectioned-properties in terms of name-values
    Enables passing of a map of maps, rather than just a vector of maps.

--- src/pallet/config_file/format.clj
+++ src/pallet/config_file/format.clj
@@ -3,21 +3,6 @@
   (:require
    [clojure.string :as string]))
 
-(defn sectioned-properties
-  "A sectioned property file.
-   This is modeled as vector of maps. The keys of the outer map are the section
-   names.  The inner maps are keyword value maps."
-  [m]
-  (letfn [(format-kv
-           [[key-name value]]
-           (format "%s = %s\n" (name key-name) value))
-          (format-section
-           [[section-name kv-map]]
-           (format
-            "[%s]\n%s\n" (name section-name)
-            (string/join (map format-kv kv-map))))]
-    (string/join (map format-section m))))
-
 (defn name-values
   "A property file.
    The properties are written \"key value\", one per line.
@@ -29,3 +14,15 @@
    (map
     (fn [[key-name value]] (format "%s%s%s\n" (name key-name) separator value))
     m)))
+
+(defn sectioned-properties
+  "A sectioned property file.
+   This is modeled as a map of maps. The keys of the outer map are the section
+   names.  The inner maps are keyword value maps."
+  [m & {:keys [separator] :or {separator " = "}}]
+  (letfn [(format-section
+           [[section-name kv-map]]
+           (format
+            "[%s]\n%s\n" (name section-name)
+                         (name-values kv-map :separator separator)))]
+    (string/join (map format-section m))))
