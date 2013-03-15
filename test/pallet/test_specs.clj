00db156b458066dcb379d3b037a7627407e7835f
Hugo Duncan <hugo@hugoduncan.org>
2013-03-15 14:54:39 -0400

    Enable integration tests with lein pallet up

---
+++ test/pallet/test_specs.clj
@@ -0,0 +1,56 @@
+(ns pallet.test-specs
+  "Various group specs to test pallet on live infrastructure."
+  (:require
+   [clojure.java.io]
+   [pallet.actions :refer [delete-local-path directory exec-checked-script
+                           exec-script* remote-file]]
+   [pallet.api :refer [group-spec plan-fn]]
+   [pallet.crate :refer [admin-user defplan]]
+   [pallet.script-test :refer [testing-script is-true is=]]
+   [pallet.utils :refer [tmpfile]]))
+
+(def chars (apply vector "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"))
+(defn rand-char [] (chars (rand-int (count chars))))
+(defn rand-str [n] (apply str (take n (repeatedly rand-char))))
+
+(def remote-file-test
+  (let [s (rand-str 10)]
+    (group-spec "remote-file"
+      :phases {:configure (plan-fn
+                            (let [f (tmpfile)
+                                  u (:username (admin-user))]
+                              (spit f s)
+                              (directory "some/path")
+                              (remote-file "some/path/file"
+                                           :local-file (.getPath f)
+                                           :owner u)
+                              (directory "/var/lib")
+                              (remote-file "/var/lib/file"
+                                           :local-file (.getPath f))
+                              (delete-local-path (.getPath f))))
+               :test (plan-fn
+                       (let [u (:username (admin-user))]
+                         (exec-script*
+                          (testing-script
+                           "remote-file"
+                           (is-true
+                            (file-exists? "some/path/file")
+                            "local-file copied correctly")
+                           (is=
+                            ~u
+                            @("stat" "-c" "%U" "some/path/file")
+                            "local-file has correct ownership")
+                           (is-true
+                            (file-exists?
+                             (str "/var/lib/pallet/admin-home/" ~u
+                                  "/some/path/file"))
+                            "local-file copied correctly to parallel tree")
+                           (is-true
+                            (file-exists?
+                             (str "/var/lib/pallet/admin-home/" ~u
+                                  "/some/path/file.md5"))
+                            "local-file generated md5 in parallel tree")
+                           (is-true
+                            (file-exists? "/var/lib/file")
+                            "local-file copied correctly")))))}
+      :roles #{:live-test :remote-file})))
