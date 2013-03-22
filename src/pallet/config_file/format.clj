e8e37f7ab3dea8fe64119815607d954c4430dec1
Pepijn de Vos <pepijndevos@gmail.com>
2013-03-22 20:07:13 +0100

    Change the order of the config functions

--- src/pallet/config_file/format.clj
+++ src/pallet/config_file/format.clj
@@ -3,18 +3,6 @@
   (:require
    [clojure.string :as string]))
 
-(defn sectioned-properties
-  "A sectioned property file.
-   This is modeled as a map of maps. The keys of the outer map are the section
-   names.  The inner maps are keyword value maps."
-  [m & {:keys [separator] :or {separator " = "}}]
-  (letfn [(format-section
-           [[section-name kv-map]]
-           (format
-            "[%s]\n%s\n" (name section-name)
-                         (name-values kv-map :separator separator)))]
-    (string/join (map format-section m))))
-
 (defn name-values
   "A property file.
    The properties are written \"key value\", one per line.
@@ -26,3 +14,15 @@
    (map
     (fn [[key-name value]] (format "%s%s%s\n" (name key-name) separator value))
     m)))
+
+(defn sectioned-properties
+  "A sectioned property file.
+   This is modeled as a map of maps. The keys of the outer map are the section
+   names.  The inner maps are keyword value maps."
+  [m & {:keys [separator] :or {separator " = "}}]
+  (letfn [(format-section
+           [[section-name kv-map]]
+           (format
+            "[%s]\n%s\n" (name section-name)
+                         (name-values kv-map :separator separator)))]
+    (string/join (map format-section m))))
