fc169201e45052069e293235c65032b0cbe99723
Hugo Duncan <hugo@hugoduncan.org>
2013-04-02 11:11:38 -0400

    Update to pallet-fsmop 0.2.6
    Fixes hangs caused by assertion failures.

--- project.clj
+++ project.clj
@@ -25,7 +25,7 @@ unprecedented level of customization."
                  [com.palletops/stevedore "0.8.0-beta.2"]
                  [enlive "1.0.1"
                   :exclusions [org.clojure/clojure]]
-                 [pallet-fsmop "0.2.6-SNAPSHOT"]
+                 [pallet-fsmop "0.2.6"]
                  [pallet-map-merge "0.1.0"]
                  [useful "0.8.6"]
                  [commons-codec "1.4"]]
