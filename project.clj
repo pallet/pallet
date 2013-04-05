f3892b0584941c12b8a9605215d71bd022025498
Hugo Duncan <hugo@hugoduncan.org>
2013-04-05 18:14:36 -0400

    Update to script-exec 0.3.1
    Should fix issues with stale jsch Session objects being re-used.

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@ unprecedented level of customization."
                  [com.palletops/chiba "0.2.0"]
                  [com.palletops/thread-expr "1.3.0"]
                  [com.palletops/pallet-common "0.4.0"]
-                 [com.palletops/script-exec "0.3.0"]
+                 [com.palletops/script-exec "0.3.1"]
                  [com.palletops/stevedore "0.8.0-beta.2"]
                  [enlive "1.0.1"
                   :exclusions [org.clojure/clojure]]
