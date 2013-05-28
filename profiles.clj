46ab3fc8fab2595802a5d3af0e7c6c79b92a494c
Hugo Duncan <hugo@hugoduncan.org>
2013-05-28 11:41:56 -0400

    Fix no-checkouts profile

--- profiles.clj
+++ profiles.clj
@@ -22,7 +22,7 @@
  {:plugins [[lein-set-version "0.2.1"]]
   :set-version
   {:updates [{:path "README.md" :no-snapshot true}]}}
- :no-checkouts {:checkout-shares ^:replace []} ; disable checkouts
+ :no-checkouts {:checkout-deps-shares ^:replace []} ; disable checkouts
  :clojure-1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}
  :jclouds {:repositories
            {"sonatype"
