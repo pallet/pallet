bac1c7a223b9174ae4eaac7552edce31a4239150
Hugo Duncan <hugo@hugoduncan.org>
2013-08-30 05:56:47 -0400

    Add remote-directory live test
    Test remote-directory with a local zip file.
    Can be run with:
    
    lein with-profile +pallet-lein,+vmfest pallet up --roles remote-directory

--- profiles.clj
+++ profiles.clj
@@ -33,4 +33,5 @@
                           [org.jclouds.driver/jclouds-slf4j "1.5.5"
                            :exclusions [org.slf4j/slf4j-api]]
                           [org.jclouds.driver/jclouds-sshj "1.5.5"]]}
- :vmfest {:dependencies [[com.palletops/pallet-vmfest "0.3.0-alpha.5"]]}}
+ :vmfest {:dependencies [[com.palletops/pallet-vmfest "0.3.0-alpha.5"]]}
+ :pallet-lein {:plugins [[com.palletops/pallet-lein "0.8.0-alpha.1"]]}}
