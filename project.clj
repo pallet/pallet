c32b95bd6dfe2bc3625520438d6eb86e05e6643a
Hugo Duncan <hugo@hugoduncan.org>
2013-09-20 13:59:07 -0400

    Update to stevedore 0.8.0-beta.5

--- project.clj
+++ project.clj
@@ -24,7 +24,7 @@ unprecedented level of customization."
                  [com.palletops/pallet-repl "0.8.0-beta.2"
                   :exclusions [com.palletops/pallet]]
                  [com.palletops/script-exec "0.4.0"]
-                 [com.palletops/stevedore "0.8.0-beta.4"]
+                 [com.palletops/stevedore "0.8.0-beta.5"]
                  [enlive "1.0.1"
                   :exclusions [org.clojure/clojure]]
                  [pallet-fsmop "0.3.1"]
