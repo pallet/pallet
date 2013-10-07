9626302a491196a5200de64b7149e21d70e0d740
Hugo Duncan <hugo@hugoduncan.org>
2013-10-07 18:14:19 -0400

    Exclude clojurescript transitive dependencies

--- project.clj
+++ project.clj
@@ -14,12 +14,14 @@ unprecedented level of customization."
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.incubator "0.1.0"]
-                 [org.clojure/core.typed "0.2.14-SNAPSHOT"]
+                 [org.clojure/core.typed "0.2.14-SNAPSHOT"
+                  :exclusions [org.clojure/clojurescript]]
                  [org.clojure/tools.logging "0.2.0"]
                  [org.clojure/tools.macro "0.1.1"]
                  [org.clojure/tools.cli "0.2.2"]
                  [org.clojure/algo.monads "0.1.0"]
-                 [org.clojure/core.async "0.1.222.0-83d0c2-alpha"]
+                 [org.clojure/core.async "0.1.222.0-83d0c2-alpha"
+                  :exclusions [org.clojure/clojurescript]]
                  [com.palletops/chiba "0.2.0"]
                  [com.palletops/thread-expr "1.3.0"]
                  [com.palletops/pallet-common "0.4.0"]
