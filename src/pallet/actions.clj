e24d71fbc1b51f0fa6db7750c2f37cb70589daf5
Antoni Batchelli <tbatchelli@acm.org>
2013-11-07 18:00:54 -0800

    Have remote-file and remote-directory honor :sudo-user. Fixes #295

--- src/pallet/actions.clj
+++ src/pallet/actions.clj
@@ -494,7 +494,7 @@ Content can also be copied from a blobstore.
       (transfer-file local-file new-path md5-path))
     ;; we run as root so we don't get permission issues
     (with-action-options (merge
-                          {:script-prefix :sudo :sudo-user nil}
+                          {:script-prefix :sudo :sudo-user (:sudo-user (admin-user))}
                           local-file-options)
       (remote-file-action
        path
@@ -623,7 +623,7 @@ only specified files or directories, use the :extract-files option.
       (transfer-file local-file new-path md5-path))
     ;; we run as root so we don't get permission issues
     (with-action-options (merge
-                          {:script-prefix :sudo :sudo-user nil}
+                          {:script-prefix :sudo :sudo-user (:sudo-user (admin-user))}
                           local-file-options)
       (remote-directory-action
        path
