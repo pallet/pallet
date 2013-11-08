75bf3db0e5a1e635960e7117d7089d86943a75a8
Hugo Duncan <hugo@hugoduncan.org>
2013-11-08 11:34:31 -0500

    Fix sudo user in rsync
    Ensure the sudo user from the admin-user or action-options is used
    when executing rsync.

--- src/pallet/actions/direct/rsync.clj
+++ src/pallet/actions/direct/rsync.clj
@@ -4,15 +4,19 @@
    [pallet.action :refer [implement-action]]
    [pallet.actions :refer [rsync*]]
    [pallet.node :refer [primary-ip ssh-port]]
-   [pallet.stevedore :as stevedore]))
+   [pallet.script.lib :refer [sudo]]
+   [pallet.stevedore :as stevedore :refer [fragment]]))
 
 (def ^{:private true}
   cmd "/usr/bin/rsync -e '%s' -F -F %s %s %s@%s:%s")
 
-(def default-options {:r true :delete true :copy-links true
-                      :rsync-path "sudo rsync"
-                      :owner true
-                      :perms true})
+(defn default-options
+  [action-options]
+  {:r true :delete true :copy-links true
+   :rsync-path (let [sudo-user (or (:sudo-user action-options))]
+                 (fragment ((sudo :no-promt true :user ~sudo-user) "rsync")))
+   :owner true
+   :perms true})
 
 (implement-action rsync* :direct {:action-type :script :location :origin}
   [action-options from to {:keys [ip username port] :as options}]
@@ -23,7 +27,8 @@
                    (format "-p %s" port)))
         cmd (format
              cmd ssh
-             (stevedore/map-to-arg-string (merge default-options extra-options))
+             (stevedore/map-to-arg-string
+              (merge (default-options action-options) extra-options))
              from username ip to)]
     [{:language :bash}
      (stevedore/checked-commands (format "rsync %s to %s" from to) cmd)]))
