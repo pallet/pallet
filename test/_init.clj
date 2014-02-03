b7ae046f4064960d0406d0fb2c3b0647c0d9a49e
Hugo Duncan <hugo@hugoduncan.org>
2014-02-03 15:02:01 -0500

    Fix silent failures of chown in state-root
    Failures in chown when reflecting the permissions of the file system
    tree into state-root were not being reported.

--- test/_init.clj
+++ test/_init.clj
@@ -3,13 +3,14 @@
   (:require
    [clojure.test :refer :all]
    [pallet.action :refer [with-action-options]]
-   [pallet.actions :refer [directory]]
+   [pallet.actions :refer [directory exec-checked-script]]
    [pallet.algo.fsmop :refer [failed?]]
    [pallet.api :refer [group-spec lift plan-fn]]
    [pallet.common.logging.logutils :refer [logging-threshold-fixture]]
    [pallet.core.api :refer [phase-errors]]
    [pallet.crate :refer [admin-user]]
-   [pallet.script.lib :refer [file state-root]]
+   [pallet.script.lib
+    :refer [chown file mkdir path-owner state-root user-home]]
    [pallet.stevedore :refer [fragment]]
    [pallet.test-utils :refer [make-localhost-compute]]))
 
@@ -20,10 +21,29 @@
         op (lift
             (group-spec "local")
             :phase (plan-fn
-                     (with-action-options {:script-prefix :sudo}
-                       (directory (fragment (file (state-root) "pallet"))
-                                  :owner (:username (admin-user))
-                                  :recursive false)))
+                    (with-action-options {:script-prefix :sudo
+                                          :script-env-fwd [:TMPDIR]}
+                      ;; (exec-checked-script
+                      ;;  (str "Ensure " (state-root) " exists")
+                      ;;  (if-not (directory? ~(state-root))
+                      ;;    (do
+                      ;;      (mkdir -p ~(state-root)))))
+                      (directory
+                       (fragment (file (state-root) "pallet")))
+                      (directory
+                       (fragment (file (state-root) "pallet"
+                                       (user-home ~(:username (admin-user)))))
+                       :owner (:username (admin-user))
+                       :recursive false)
+                      (exec-checked-script
+                       "Ensure TMPDIR"
+                       (if (&& (directory? @TMPDIR)
+                               (== @(path-owner @TMPDIR)
+                                   ~(:username (admin-user))))
+                         (do
+                           (set! tpath (file (state-root) "pallet" @TMPDIR))
+                           (mkdir @tpath :path true)
+                           (chown ~(:username (admin-user)) @tpath))))))
             :compute compute
             :async true)
         session @op]
