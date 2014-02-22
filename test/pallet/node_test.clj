cf9b1f75fce3ea415f6caa3564a0cc33b555320e
Hugo Duncan <hugo@hugoduncan.org>
2014-02-22 17:56:19 -0500

    Add a has-base-name? test

--- test/pallet/node_test.clj
+++ test/pallet/node_test.clj
@@ -1,8 +1,8 @@
 (ns pallet.node-test
   (:require
    [clojure.test :refer :all]
-   [pallet.compute.node-list :refer [make-node node]]
-   [pallet.node :refer [node-address]]))
+   [pallet.compute.node-list :as node-list :refer [make-node node]]
+   [pallet.node :as node :refer [node-address]]))
 
 (deftest node-address-test
   (let [ip "1.2.3.4"]
@@ -10,3 +10,11 @@
     (is (= ip (node-address
                (make-node nil :g nil nil {:id "id" :private-ip ip}))))
     (is (= ip (node-address (node "mynode" {:ip ip}))))))
+
+(deftest has-base-name?-test
+  (is (node/has-base-name?
+       (node-list/node "mynode" {:ip "localhost"})
+       "mynode"))
+  (is (node/has-base-name?
+       (node-list/node "mynode" {:ip "localhost"})
+       :mynode)))
