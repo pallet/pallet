b7a7eedee5ab068731abfbe1e122247b52a512f2
Hugo Duncan <hugo@hugoduncan.org>
2014-02-21 22:06:03 -0500

    Clean up pallet.session

--- src/pallet/session.clj
+++ src/pallet/session.clj
@@ -56,20 +56,7 @@ The session is a map with well defined keys:
    [pallet.node :as node]
    [pallet.user :refer [user?]]
    [pallet.utils :as utils]
-   ;; [circle.schema-typer :refer [def-schema-alias def-validator]]
    [schema.core :as schema :refer [check required-key optional-key validate]]))
-
-;; (defmethod circle.schema-typer/convert :default [s]
-;;   (if (instance? Class s)
-;;     (symbol (.getName s))
-;;     (assert nil (str "Don't know how to convert " (pr-str s)))))
-
-;; (defmethod circle.schema-typer/convert nil [s]
-;;   nil)
-
-;; (defmethod circle.schema-typer/convert clojure.lang.PersistentList [s]
-;;   (assert nil (str "Don't know how to convert " s))
-;;   (symbol (.getName s)))
 
 (ann execution-state (HMap))
 (def execution-state
@@ -80,9 +67,6 @@ The session is a map with well defined keys:
    (optional-key :environment) schema/Any
    (optional-key :extension) {schema/Keyword schema/Any}})
 
-;; (def-schema-alias ExecutionState execution-state)
-;; (def-validator validate-execution-state ExecutionState execution-state)
-
 (ann ^:no-check base-session (HMap))
 (def base-session
   {:execution-state execution-state
@@ -92,14 +76,11 @@ The session is a map with well defined keys:
 (def target-session
   (assoc base-session :target {schema/Keyword schema/Any}))
 
-
 (ann ^:no-check base-session? (predicate BaseSession))
 (defn base-session?
   [x]
   (not (schema/check base-session x)))
 
-
-;; (ann ^:no-check target-session? (predicate BaseSession))
 (defn target-session?
   [x]
   (not (schema/check target-session x)))
@@ -107,10 +88,6 @@ The session is a map with well defined keys:
 (defn validate-target-session
   [x]
   (schema/validate target-session x))
-
-
-;; (def-schema-alias BaseSession base-session)
-;; (def-validator validate-base-session BaseSession base-session)
 
 (ann create [(HMap
               :mandatory {:executor Executor}
@@ -136,83 +113,17 @@ The session is a map with well defined keys:
      {:plan-state plan-state})))
 
 
-;; ;; Using the session var directly is to be avoided. It is a dynamic var in
-;; ;; order to provide thread specific bindings. The value is expected to be an
-;; ;; atom to allow in-place update semantics.
-;; ;; TODO - remove :no-check
-;; (ann ^:no-check *session* (Atom1 Session))
-;; (def ^{:internal true :dynamic true :doc "Current session state"}
-;;   *session*)
-
-;; ;;; # Session map low-level API
-;; ;;; The aim here is to provide an API that could possibly be backed by something
-;; ;;; other than a plain map.
-
-;; ;; TODO - remove :no-check
-;; (ann ^:no-check session [-> Session])
-;; (defn session
-;;   "Return the current session, which implements clojure's map interfaces."
-;;   []
-;;   (assert (bound? #'*session*)
-;;           "Session not bound.  The session is only bound within a phase.")
-;;   ;; (thread-local *session*)
-;;   @*session*
-;;   )
-
-;; (defmacro ^{:requires [#'with-thread-locals]}
-;;   with-session
-;;   [session & body]
-;;   `(binding [*session* (atom ~session)]
-;;      ~@body))
-
-;; (ann session! [Session -> Session])
-;; (defn session!
-;;   [session]
-;;   (reset! *session* session))
-
-;; ;;; ## Session modifiers
-;; (defn assoc-session!
-;;   "Assoc key value pairs in the session."
-;;   [& kvs]
-;;   (apply swap! *session* assoc kvs))
-
-;; (defn update-in-session!
-;;   "Assoc key value pairs in the session."
-;;   [ks f args]
-;;   (clojure.tools.logging/debugf "update-in-session! %s %s %s" ks f args)
-;;   (apply swap! *session* update-in ks f args))
-
-;; (defn plan-state!
-;;   "Set the plan state"
-;;   [m]
-;;   {:pre [(satisfies? pallet.core.plan-state.protocols/StateGet m)]}
-;;   (assoc-session! :plan-state m))
-
 (ann plan-state [BaseSession -> PlanState])
 (defn plan-state
   "Get the plan state"
   [session]
   (:plan-state session))
 
-;; (defn recorder!
-;;   "Set the action recorder"
-;;   [f]
-;;   (assoc-session! :recorder f))
-
 (ann recorder [BaseSession -> Recorder])
 (defn recorder
   "Get the action recorder"
   [session]
   (-> session :execution-state :recorder))
-
-;; (defmacro with-recorder
-;;   "Set the recorder for the scope of the body."
-;;   [recorder & body]
-;;   `(let [r# (recorder)]
-;;      (try
-;;        (recorder! ~recorder)
-;;        ~@body
-;;        (finally (recorder! r#)))))
 
 (ann executor [BaseSession -> Executor])
 (defn executor
@@ -222,31 +133,12 @@ The session is a map with well defined keys:
    :post [(executor? %)]}
   (-> session :execution-state :executor))
 
-;; TODO this is an inconsistent set- function, as it isn't mutating.
-;; Consider ! decorating the other setters?
 (ann set-executor [BaseSession Executor -> BaseSession])
 (defn set-executor
   "Get the action executor."
   [session executor]
   {:pre [(executor? executor)]}
   (assoc-in session [:execution-state :executor] executor))
-
-;; (ann execute-status-fn [BaseSession -> ExecuteStatusFn])
-;; (defn execute-status-fn
-;;   "Get the action execute status function"
-;;   [session]
-;;   {:post [(fn? %)]}
-;;   (-> session :execution-state :execute-status-fn))
-
-;; (defn executor!
-;;   "Set the action executor"
-;;   [f]
-;;   (assoc-session! :executor f))
-
-;; (defn execute-status-fn!
-;;   "Set the action execute status function"
-;;   [f]
-;;   (assoc-session! :execute-status-fn f))
 
 (ann action-options [BaseSession -> HMap])
 (defn action-options
@@ -271,7 +163,6 @@ The session is a map with well defined keys:
   [session f args]
   (apply update-in session [:execution-state :environment] f args))
 
-
 (defn ^:internal extension
   "Get the extension data for the specified keyword."
   [session extension-kw]
@@ -287,12 +178,7 @@ The session is a map with well defined keys:
   [session extension-kw value]
   (assoc-in session [:execution-state :extension extension-kw] value))
 
-
-(def-alias SessionModifier
-  (TFn [[t :variance :contravariant]]
-       (All [[x :< BaseSession]] (Fn [x t -> x]))))
-
-(ann ^:no-check set-user [BaseSession User -> BaseSession])
+(ann ^:no-check user [BaseSession -> User])
 (defn user
   "Return a session with `user` as the known admin user."
   [session]
