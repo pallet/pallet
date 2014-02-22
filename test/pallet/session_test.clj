e006821188e8297418a5bd380ea248cfd3bd7975
Hugo Duncan <hugo@hugoduncan.org>
2014-02-21 22:08:28 -0500

    Make pallet.session tests pass

--- test/pallet/session_test.clj
+++ test/pallet/session_test.clj
@@ -3,14 +3,13 @@
    [clojure.core.typed
     :refer [check-ns fn>]]
    [clojure.test :refer :all]
+   [pallet.core.executor.ssh :refer [ssh-executor]]
    [pallet.core.plan-state.in-memory :refer [in-memory-plan-state]]
    [pallet.core.recorder.in-memory :refer [in-memory-recorder]]
    [pallet.core.types :refer [Session Action ActionResult]]
    [pallet.session :refer [create]]))
 
 (deftest create-test
-  (is (create {:executor (fn> [session :- Session action :- Action]
-                           {:exit 0})
-               :execute-status-fn (fn> [r :- ActionResult] r)
+  (is (create {:executor  (ssh-executor)
                :plan-state (in-memory-plan-state {})
                :recorder (in-memory-recorder)})))
