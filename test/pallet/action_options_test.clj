e83218b49499a47c6d6affbf8fe72df5c63ea557
Hugo Duncan <hugo@hugoduncan.org>
2014-02-23 05:50:21 -0500

    Add pallet.acion-options test

---
+++ test/pallet/action_options_test.clj
@@ -0,0 +1,14 @@
+(ns pallet.action-options-test
+  (:require
+   [clojure.test :refer :all]
+   [pallet.action-options :refer :all]
+   [pallet.core.executor.plan :as plan]
+   [pallet.session :as session]))
+
+(deftest with-action-options-tets
+  (let [session (session/create {:executor (plan/executor)})
+        m {:kw 1}]
+    (is (empty? (action-options session)))
+    (with-action-options session m
+      (is (= m (action-options session))))
+    (is (empty? (action-options session)))))
