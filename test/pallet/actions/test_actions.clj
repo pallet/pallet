17a480508ce0f0a493b5f74bf5b710a18b7a0ad2
Hugo Duncan <hugo@hugoduncan.org>
2014-03-08 06:09:26 -0500

    Add plan executor implementation for fail action

--- test/pallet/actions/test_actions.clj
+++ test/pallet/actions/test_actions.clj
@@ -1,7 +1,10 @@
 (ns pallet.actions.test-actions
   "Actions useful in testing"
   (:require
-   [pallet.action :refer [defaction implement-action]]))
+   [pallet.action :refer [defaction implement-action]]
+   [pallet.core.executor.plan
+    :refer [action-result replace-action-with-symbol]]
+   [pallet.exception :refer [domain-info]]))
 
 (defaction fail
   "An action that always fails."
@@ -11,3 +14,11 @@
   "echo fail action; exit 1")
 
 (implement-action fail :direct {} {:language :bash} fail*)
+
+(defmethod action-result `fail
+  [target action]
+  (throw
+   (domain-info
+    "Fail action"
+    {:result (assoc (replace-action-with-symbol action)
+               :error {:message "fail action"})})))
