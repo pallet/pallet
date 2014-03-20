9d0271b7e93515991ef0069fed6b0617e94c5f64
Hugo Duncan <hugo@palletops.com>
2014-03-20 16:29:41 -0400

    Remove release script

--- profiles.clj
+++ profiles.clj
@@ -23,8 +23,7 @@
                  "codox" ["doc"]
                  "doc" ["do" "codox," "marg"]}}
  :release
- {:plugins [[lein-set-version "0.2.1"]]
-  :set-version
+ {:set-version
   {:updates [{:path "README.md" :no-snapshot true}]}}
  :no-checkouts {:checkout-deps-shares ^:replace []} ; disable checkouts
  :clojure-1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}
